a02ad8cde65667a0d062cce1d5d53d5b733ac9a1
Conscat <wgooch2000@gmail.com>
2021-11-28 17:44:04 -0500

    Converted to C, fruitlessly

--- unistd.h
+++ unistd.h
@@ -1,8 +1,11 @@
 // -*- mode: c++;-*-
 #pragma once
 
-constexpr static int EXIT_SUCCESS = 0;  // NOLINT
-constexpr static int EXIT_FAILURE = 1;  // NOLINT
+/* constexpr static int EXIT_SUCCESS = 0;  // NOLINT */
+/* constexpr static int EXIT_FAILURE = 1;  // NOLINT */
 
-using size_t = long unsigned int;
-using ssize_t = long int;
+/* using size_t = long unsigned int; */
+/* using ssize_t = long int; */
+
+typedef long unsigned int size_t;
+typedef long int ssize_t;
