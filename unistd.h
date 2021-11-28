31c0b9c58a7bbe0c48747f91abe8eb291685abc7
Conscat <omg-lily2000@gmail.com>
2021-11-28 16:42:03 -0500

    Many improvements

--- unistd.h
+++ unistd.h
@@ -1,7 +1,8 @@
 // -*- mode: c++;-*-
+#pragma once
 
 constexpr static int EXIT_SUCCESS = 0;  // NOLINT
-constexpr static int EXIT_FAILURE = 0;  // NOLINT
+constexpr static int EXIT_FAILURE = 1;  // NOLINT
 
 using size_t = long unsigned int;
 using ssize_t = long int;
