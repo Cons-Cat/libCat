566443238af881b07d299712d8302460dfe54583
Conscat <wgooch2000@gmail.com>
2021-11-28 20:49:38 -0500

    Back to C++, but still extenal asm now

--- unistd.h
+++ unistd.h
@@ -1,11 +1,10 @@
 // -*- mode: c++;-*-
 #pragma once
 
-/* constexpr static int EXIT_SUCCESS = 0;  // NOLINT */
-/* constexpr static int EXIT_FAILURE = 1;  // NOLINT */
+constexpr static int EXIT_SUCCESS = 0;  // NOLINT
+constexpr static int EXIT_FAILURE = 1;  // NOLINT
 
-/* using size_t = long unsigned int; */
-/* using ssize_t = long int; */
-
-typedef long unsigned int size_t;
-typedef long int ssize_t;
+using size_t = long unsigned int;
+using usize = size_t;
+using ssize_t = long signed int;
+using isize = ssize_t;
