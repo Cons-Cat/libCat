b646c2898fb1186e376f746ce91f7ac2c8fff1c3
Conscat <wgooch2000@gmail.com>
2021-12-01 00:58:58 -0500

    Minor simplifications

--- src/start_exit/start_exit.hpp
+++ src/start_exit/start_exit.hpp
@@ -1,14 +1,13 @@
 #pragma once
 
 // NOLINTNEXTLINE
-__attribute__((optimize(0))) void _exit(const i32& exit_code) {
+__attribute__((optimize(0))) void _exit(i32 const& exit_code) {
     // clang-format off
-	asm(R"(
-        syscall
-        ret)"
-		: // No outputs.
-		:"D" (exit_code), "a"(60)
-		: "memory");
+	asm(R"(syscall
+           ret)"
+		   : // No outputs.
+		   :"D" (exit_code), "a"(60)
+		   : "memory");
     // clang-format on
     __builtin_unreachable();
 }
@@ -16,14 +15,13 @@ __attribute__((optimize(0))) void _exit(const i32& exit_code) {
 // NOLINTNEXTLINE
 __attribute__((optimize(0))) void _start() {
     // clang-format off
-    asm(R"(
-        .global _start
-        _start:
-        xor %ebp, %ebp # Zero-out the stack pointer.
-        mov %rsp, %rdi # Setup argc
-        lea 8(%rsp), %rsi # Setup argv
-        call main
-        mov %rax, %rdi # Forward main()'s return to _exit.
-        call _exit)");
+    asm(R"(.global _start
+           _start:
+           xor %rbp, %rbp # Zero-out the stack pointer.
+           mov %rsp, %rdi # Setup argc
+           lea 8(%rsp), %rsi # Setup argv
+           call main
+           mov %rax, %rdi # Forward main()'s return to _exit.
+           call _exit)");
     // clang-format on
 }
