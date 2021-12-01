91a59ae7fb315083797322a99ffb0b622f1f8e22
Conscat <wgooch2000@gmail.com>
2021-12-01 00:31:11 -0500

    Created Result handling

---
+++ src/pthread/pthread.h
@@ -0,0 +1,55 @@
+#pragma once
+
+enum class LinuxError;
+
+// TODO: Make a locale header.
+struct locale_map;
+
+struct locale_struct {
+    const struct locale_map* cat[6];
+};
+
+using locale_t = locale_struct;
+
+struct pthread {
+    struct pthread* self;
+    usize* dtv;
+    struct pthread *prev, *next;
+    usize sysinfo;
+    usize canary;
+
+    i32 tid;
+    LinuxError errno_val;
+    volatile i32 detach_state;
+    volatile i32 cancel;
+    volatile unsigned char canceldisable, cancelasync;
+    unsigned char tsd_used     : 1;
+    unsigned char dlerror_flag : 1;
+    unsigned char* map_base;
+    usize map_size;
+    void* stack;
+    usize stack_size;
+    usize guard_size;
+    void* result;
+    struct __ptcb* cancelbuf;  // NOLINT
+    void** tsd;
+
+    struct {
+        volatile void* volatile head;
+        long off;
+        volatile void* volatile pending;
+    } robust_list;
+
+    i32 h_errno_val;
+    volatile i32 timer_id;
+    locale_t locale;
+    volatile i32 killlock[1];
+    char* dlerror_buf;
+    void* stdio_locks;
+};
+
+auto get_pthread_pointer() -> pthread* {
+    usize thread_ptr;
+    asm(R"(mov %%fs:0,%0)" : "=r"(thread_ptr));
+    return reinterpret_cast<pthread*>(thread_ptr);
+}
---
+++ src/result/result.hpp
@@ -0,0 +1,66 @@
+// -*- mode: c++;-*-
+#pragma once
+
+#include <cstdlib>
+#include <type_traits>
+#include <utility>
+
+template <typename T, typename UError>
+struct [[nodiscard]] Result {
+    // TODO: Investigate making the data and error a sum type.
+  private:
+    using DataType = std::conditional_t<std::is_void_v<T>, void*, T>;
+    DataType data;
+
+  public:
+    UError code;
+
+    Result() = delete;
+
+    // NOLINTNEXTLINE I do not want explicit constructors here.
+    constexpr Result(UError&& in_code) {
+        this->code = std::forward<UError>(in_code);
+    }
+
+    // NOLINTNEXTLINE I do not want explicit constructors here.
+    constexpr Result(DataType&& in_data) {
+        this->data = std::forward<DataType>(in_data);
+    }
+
+    // NOLINTNEXTLINE I do not want explicit constructors here.
+    constexpr Result(DataType&& in_data, UError&& in_code) {
+        this->data = std::forward<DataType>(in_data);
+        this->code = std::forward<UError>(in_code);
+    }
+
+    // TODO: Pass in the exit code.
+    auto or_panic(char const* p_error_message = "Panic!") -> T {
+        if (is_ok(this->code)) {
+            // TODO: Print the error message.
+            exit(EXIT_FAILURE);
+        } else if constexpr (!std::is_void_v<T>) {
+            return this->data;
+        }
+    }
+
+    auto or_return(DataType const& in_data) -> DataType {
+        return in_data;
+    }
+
+    // TODO: Add std::invokable concept
+    auto or_do(auto callback) {
+        if (!is_ok(this->code)) {
+            return callback();
+        } else if constexpr (!std::is_void_v<T>) {
+            if constexpr (!std::is_void_v<T>) {
+                return this->data;
+            }
+        }
+    }
+
+    // TODO: This should be removed if this becomes a sum type. It is only
+    // useful for recursion.
+    auto or_propagate() -> Result<T, UError> {
+        return *this;
+    }
+};
