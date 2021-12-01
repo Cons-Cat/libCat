d0c59a5c66ef8de2e8268a109ccc871eff49caa6
Conscat <wgooch2000@gmail.com>
2021-11-30 19:54:06 -0500

    Added more to cstdlib

--- src/cstdlib/stdlib.h
+++ src/cstdlib/stdlib.h
@@ -2,4 +2,4 @@
 
 /* This file simply forwards an #include to <cstdlib>.
  * It exists for backwards compatibility with C code. */
-#include<cstdlib>
+#include <cstdlib>
--- src/start_exit/start_exit.hpp
+++ src/start_exit/start_exit.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
 // NOLINTNEXTLINE
-__attribute__((optimize(0))) void _exit(const i32&) {
+__attribute__((optimize(0))) void _exit(const i32& exit_code) {
     // clang-format off
-	i64 exit_code = 0;
 	asm(R"(
         syscall
         ret)"
@@ -34,14 +33,5 @@ extern "C" auto syscall5(void* p_number, void* p_arg1 = nullptr,
                          void* p_arg2 = nullptr, void* p_arg3 = nullptr,
                          void* p_arg4 = nullptr, void* p_arg5 = nullptr)
     -> void*;
-
-auto write(int const& file_descriptor, char const* const& p_string_buffer,
-           usize const& string_length) -> isize {
-    return reinterpret_cast<isize>(
-        syscall5(reinterpret_cast<void*>(1),
-                 reinterpret_cast<void*>(static_cast<isize>(file_descriptor)),
-                 (void*)(p_string_buffer),
-                 reinterpret_cast<void*>(string_length), 0, 0));
-}
 
 // TODO: Overload write() with automatic string length.
