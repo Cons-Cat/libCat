8d5d2b5cb9d2092339db7ed164a34edd3dd3c97c
Conscat <omg-lily2000@gmail.com>
2021-12-04 01:56:39 -0500

    Made ultra-thin _start

--- src/start_exit/start_exit.hpp
+++ src/start_exit/start_exit.hpp
@@ -3,52 +3,56 @@
 #include <result.hpp>
 
 #include <concepts>
+#include <cstdlib>
 #include <type_traits>
 
-// template <typename T>
-// struct Result;
+/* meow() is the entry point in a libCat program, so there is no _start().
+ *
+ * This allows libCat to do some interesting things, such as making stack
+ * initialization explicit and giving the user more control over their program
+ * than a libC.
+ *
+ * That is very generally useful. Do you need to support .init and .fini, or do
+ * you use .ctor or .dtor? Or both? Or neither? POSIX and ELF require libC
+ * support all four, technically, but there is no good reason to actually do
+ * that.
+ *
+ * Does your program need to allocate a stack at all, or can you just use
+ * argv[]? Do you even have to populate argp[] at all? There are many creative
+ * liberties that open up when you rethink the basic assumption that libC is
+ * just how computers work. It's not. They're just idioms. */
+void meow();
 
-// template struct Result<void>;
-
-auto meow(i32 argc, char const* argv[], const void* argp) -> Result<void>;
-auto meow(i32 argc, char const* argv[]) -> Result<void>;
-auto meow(i32 argc) -> Result<void>;
-auto meow() -> Result<void>;
-
-void _exit(i32 exit_code) {
+/* In libCat, the exit() function is provided globally instead of in <cstdlib>.
+ * This streamlines out the existence of _exit(). */
+void exit(i32 exit_code = EXIT_SUCCESS) {
     // clang-format off
-        asm (R"(syscall)"
+    asm (R"(syscall)"
            : // No outputs.
            : "D" (std::decay_integral(exit_code)), "a"(60)
            : "memory");
     // clang-format on
-    __builtin_unreachable();  // This improves -O0.
+    __builtin_unreachable();
 }
 
-/* extern "C" prevents C++ name mangling, which is required for the following
- * functions to link correcty. */
-
-/* _start() is the entry point in a LibCat program.
- * I choose not to support .init, .fini, .ctor, or .dtor.
- * Thus, this is not a POSIX / ELF compliant alternative to
- * the LibC _start, but this is technically reasonable. */
+/* In libCat, _start() performs no initialization code. It simply invokes
+ * meow(), and subsequently exits. This only exists to streamline the build
+ * systems and to automate exiting. */
 extern "C" void _start() {
-    // clang-format off
-    asm(R"(xor %rbp, %rbp # Zero-out the stack pointer.
-           mov %esp, %edi # Copy 32-bit argc
-           lea 8(%rsp), %rsi # Load address to argv
-/* LibCat user programs enter in meow() rather than main(). This is
- * done so that they may return a Result instead of an int. */
-        )");
-    // clang-format on
-
-    // TODO: Output %esp and %edi into variables that are passed into meow().
-    _exit(meow().code.code);
-    __builtin_unreachable();  // This improves -O0.
+    meow();
+    exit();
+    //     // clang-format off
+    //     asm(R"(xor %rbp, %rbp # Zero-out the stack pointer.
+    //            mov %esp, %edi # Copy 32-bit argc
+    //            lea 8(%rsp), %rsi # Load address to argv
+    // /* LibCat user programs enter in meow() rather than main(). This is
+    //  * done so that they may return a Result instead of an int. */
+    //         )");
+    //     // clang-format on
 }
 
 /* __stack_chk_fail() is called when stack overflow occurs in programs compiled
  * without -fno-stack-protector. */
 extern "C" void __stack_chk_fail() {
-    _exit(1);
+    exit(1);
 }
