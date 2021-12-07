bf30ce7c0971037af425cdffff1684fa840ae4c7
Conscat <omg-lily2000@gmail.com>
2021-12-07 01:35:59 -0500

    Fixed echo

--- examples/echo.cpp
+++ examples/echo.cpp
@@ -1,18 +1,17 @@
 #include <any>
-#include <cstdlib>
 #include <cunistd>
 
 void meow() {
     std::any rbp = load_base_stack_pointer();
     i32 argc = *std::any_cast<i32*>(rbp);
-    char** argv = std::any_cast<char**>(rbp);
+    char** argv = std::any_cast<char**>(rbp) + 1;
     for (i32 i = 1; i < argc; i++) {
         i32 len = 0;
         while (argv[i][len] != 0) {
             len++;
         }
         argv[i][len] = ' ';
-        write(1, argv[i][len], 1).or_panic();
+        write(1, argv[i], len + 1).or_panic();
     }
     write(1, "\n", 1).or_panic();
 }
