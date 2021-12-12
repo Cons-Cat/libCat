da9fdd8810e7ab7cb43e5123859afbe435c20b33
Conscat <wgooch2000@gmail.com>
2021-12-12 01:08:29 -0500

    Cleanup unistd.h

--- src/libraries/unistd/unistd.h
+++ src/libraries/unistd/unistd.h
@@ -1,11 +1,9 @@
 #pragma once
 
 #include <any.h>
-#include <concepts.h>
-#include <errno.h>
 
-/* For some reason, writing these with inline assembly produces incredibly
- * strange codegen. */
+/* For some reason, writing these syscalls with inline assembly produces bizarre
+ * and broken codegen. */
 namespace std::detail {
 
 /* The source to these functions is contained in the ./detail/ subdirectory at
@@ -21,31 +19,28 @@ extern "C" auto syscall5(Any, Any, Any, Any, Any arg4, Any) -> void*;
 
 }  // namespace std::detail
 
+// TODO: Extract this to a .tpp file.
 template <typename T, typename... Args>
 void syscall(T, Args... parameters) {
     constexpr i64 stdout = 1;
     constexpr usize length = sizeof...(Args);
     Any arguments[length] = {parameters...};
-    static_assert(length < 6);
+    static_assert(length < 6,
+                  "Syscalls with more than 5 arguments are not supported!");
 
     using namespace std::detail;
     if constexpr (length == 0) {
         syscall0(stdout);
-    }
-    if constexpr (length == 1) {
+    } else if constexpr (length == 1) {
         syscall1(stdout, arguments[0]);
-    }
-    if constexpr (length == 2) {
+    } else if constexpr (length == 2) {
         syscall2(stdout, arguments[0], arguments[1]);
-    }
-    if constexpr (length == 3) {
+    } else if constexpr (length == 3) {
         syscall3(stdout, arguments[0], arguments[1], arguments[2]);
-    }
-    if constexpr (length == 4) {
+    } else if constexpr (length == 4) {
         syscall4(stdout, arguments[0], arguments[1], arguments[2],
                  arguments[3]);
-    }
-    if constexpr (length == 5) {
+    } else if constexpr (length == 5) {
         syscall5(stdout, arguments[0], arguments[1], arguments[2], arguments[3],
                  arguments[4]);
     }
