363f5dba750ed8498d58d1ea6e0a0106dc310abf
Conscat <wgooch2000@gmail.com>
2021-12-13 16:09:52 -0500

    Fixed move and forward helpers

--- src/libraries/utility/utility.h
+++ src/libraries/utility/utility.h
@@ -3,17 +3,17 @@
 #include <type_traits.h>
 
 template <typename T>
-constexpr auto move(T&& input) -> std::remove_reference_t<T> {
+constexpr auto move(T&& input) -> std::remove_reference_t<T>&& {
     return static_cast<std::remove_reference_t<T>&&>(input);
 }
 
 template <typename T>
-[[nodiscard]] constexpr auto forward(std::remove_reference_t<T>& input) -> T&& {
+constexpr auto forward(std::remove_reference_t<T>& input) -> T&& {
     return static_cast<T&&>(input);
 }
 
 template <typename T>
-[[nodiscard]] constexpr auto forward(std::remove_reference_t<T>&& input)
-    -> T&& {
+constexpr auto forward(std::remove_reference_t<T>&& input) -> T&& {
+    static_assert(!std::is_lvalue_reference_v<T>);
     return static_cast<T&&>(input);
 }
