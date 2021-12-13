16cf5f3288dcb4a13d433fbb1a0edf17db58c2df
Conscat <wgooch2000@gmail.com>
2021-12-13 01:45:30 -0500

    Added unit testing framework

---
+++ tests/result_failure.cpp
@@ -0,0 +1,16 @@
+enum MyErrors
+{
+    OK = 0,
+    FAIL = 1,
+};
+
+auto result_factory(MyErrors error) -> Result<> {
+    if (error == ::OK) {
+        return ok;
+    }
+    return Failure(error);
+}
+
+void meow() {
+    result_factory(::FAIL).or_panic();
+}
