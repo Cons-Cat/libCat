58be14769590409b032fe45cf776ef4a77d59cf2
Conscat <wgooch2000@gmail.com>
2021-12-20 02:32:35 -0500

    Added simd:: namespace

--- src/libraries/string/string.h
+++ src/libraries/string/string.h
@@ -25,22 +25,35 @@ strlen(char8_t const* p_string) -> size_t {
     return string_length_as<size_t>(p_string);
 }
 
-/* T is the return type of simd_string_length(). It may be signed or unsigned.
- * This function requires SSE4.2 */
+void copy_memory(void* source, void* destination, usize bytes) {
+    if (bytes <= 16) {
+        // __builtin_memcpy(source, destination, bytes);
+    }
+}
+
+// TODO: Move into a <bit.h> library.
+auto is_aligned(void const volatile* pointer, usize byte_alignment) -> bool {
+    return (reinterpret_cast<usize>(pointer) % byte_alignment) == 0;
+}
+
+namespace simd {
+
+/* T is the return type of string_length_as(). It may be signed or
+ * unsigned. This function requires SSE4.2 */
 template <typename T>
-auto simd_string_length_as(char8_t const* p_string) -> T {
+auto string_length_as(char8_t const* p_string) -> T {
     // TODO: Align pointers.
     T result = 0;
     charx16* p_memory = p_string_to_p_vector<16>(p_string);
-    constexpr charx16 zeroes = simd_set_zeros<charx16>();
+    constexpr charx16 zeroes = simd::set_zeros<charx16>();
 
     while (true) {
         charx16 data;
         data = *p_memory;
         constexpr u8 mask =
-            ::SIDD_UBYTE_OPS | ::SIDD_CMP_EQUAL_EACH | ::SIDD_LEAST_SIGNIFICANT;
-        if (simd_cmp_implicit_str_c<mask>(data, zeroes)) {
-            i64 const index = simd_cmp_implicit_str_i<mask>(data, zeroes);
+            SIDD_UBYTE_OPS | SIDD_CMP_EQUAL_EACH | SIDD_LEAST_SIGNIFICANT;
+        if (simd::cmp_implicit_str_c<mask>(data, zeroes)) {
+            i64 const index = simd::cmp_implicit_str_i<mask>(data, zeroes);
             return result + index;
         }
         p_memory++;
@@ -51,12 +64,4 @@ auto simd_string_length_as(char8_t const* p_string) -> T {
     __builtin_unreachable();
 }
 
-auto is_aligned(void const volatile* pointer, usize byte_alignment) -> bool {
-    return (reinterpret_cast<usize>(pointer) % byte_alignment) == 0;
-}
-
-void copy_memory(void* source, void* destination, usize bytes) {
-    if (bytes <= 16) {
-        // __builtin_memcpy(source, destination, bytes);
-    }
-}
+}  // namespace simd
--- tests/string_length.cpp
+++ tests/string_length.cpp
@@ -5,6 +5,6 @@ void meow() {
     char8_t const* p_string = u8"Hello!";
     debugger_entry_point();
     i32 scalar_len = string_length_as<i32>(p_string);
-    i32 vector_len = simd_string_length_as<i32>(p_string);
+    i32 vector_len = simd::string_length_as<i32>(p_string);
     mu_assert(scalar_len == vector_len, "SIMD string length is incorrect!");
 }
