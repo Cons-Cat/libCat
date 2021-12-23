a0f62381f11e1e7931644779715877fe657ecdda
Conscat <omg-lily2000@gmail.com>
2021-12-23 02:21:17 -0500

    Made copy_memory test more rigorous

--- tests/copy_memory.cpp
+++ tests/copy_memory.cpp
@@ -4,6 +4,10 @@
 void meow() {
     debugger_entry_point();
     Buffer<i4, 200> source_200;
+    for (i4 i = 0; i < 200; i++) {
+        source_200[i] = i;
+    }
+
     Buffer<i4, 200> dest_200;
     copy_memory(&dest_200, &source_200, sizeof(dest_200));
     for (i4 i = 0; i < 200; i++) {
@@ -11,6 +15,10 @@ void meow() {
     }
 
     Buffer<i4, 2000> source_2000;
+    for (i4 i = 0; i < 2000; i++) {
+        source_2000[i] = i;
+    }
+
     Buffer<i4, 2000> dest_2000;
     for (i4 i = 0; i < 2000; i++) {
         source_2000[i] = 1;
