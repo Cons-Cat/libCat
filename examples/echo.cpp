be6ef16cdc0f8945b29da826d9a1a4ed3b2bf361
Conscat <omg-lily2000@gmail.com>
2021-12-24 16:50:18 -0500

    Minor changes

--- examples/echo.cpp
+++ examples/echo.cpp
@@ -1,10 +1,11 @@
-#include <any.h>
 #include <unistd.h>
 
 void meow() {
     debugger_entry_point();
+    // TODO: I think the `Any` refactor caused this to segfault.
     Any rbp = load_base_stack_pointer();
     i4 argc = rbp;
+    // TODO: Use `load_argv()`
     char8_t** argv = (char8_t**)(rbp) + 1;
     for (i4 i = 1; i < argc; i++) {
         i4 length = 0;
