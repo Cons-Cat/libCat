a9090784968c91d5ba609a66d0fa348f0d06be0f
Conscat <wgooch2000@gmail.com>
2021-12-25 23:59:18 -0500

    Fixed many issues related to syscalls and mmap

--- src/libraries/unistd/unistd.h
+++ src/libraries/unistd/unistd.h
@@ -5,7 +5,7 @@
 #include <any.h>
 #include <errno.h>
 
-// TODO: Remove all trace of errno.
+// TODO: Remove all trace of `errno`.
 
 /* For some reason, writing these syscalls with inline assembly produces bizarre
  * and broken codegen. */
@@ -25,26 +25,18 @@ extern "C" auto syscall4(Any, Any, Any, Any, Any arg4) -> void*;
 extern "C" auto syscall5(Any, Any, Any, Any, Any arg4, Any) -> void*;
 auto syscall6(Any call, Any arg1, Any arg2, Any arg3, Any arg4, Any arg5,
               Any arg6) -> Result<Any> {
-    register auto a1 asm("r10") = arg1;
-    register auto a3 asm("r8") = arg3;
-    register auto a4 asm("r9") = arg4;
-
-    void* unused_output;
-    register void* unused_output2 asm("r11");
+    register Any r10 asm("r10") = arg4;
+    register Any r8 asm("r8") = arg5;
+    register Any r9 asm("r9") = arg6;
 
     Any result;
-    asm volatile(R"(sub $64, %%rsp
-                    movq %[a5], 40(%%rsp)
-                    movq %[a6], 48(%%rsp)
-                    syscall
-                    add $64, %%rsp)"
-                 : "=a"(result), "=r"(a1), "=d"(arg2), "=r"(a3), "=r"(a4),
-                   "=c"(unused_output), "=r"(unused_output2)
-                 : "a"(call), "r"(a1), "d"(arg2), "r"(a3),
-                   "r"(a4), [a5] "re"(meta::bit_cast<void*>(arg5)),
-                   // TODO: Fix this segfaulting with static_cast<void*>():
-                   [a6] "re"(meta::bit_cast<void*>(arg6))
-                 : "memory", "cc");
+    asm("syscall"
+        : "=a"(result)
+        : "a"(call), "D"(arg1), "S"(arg2), "d"(arg3), "r"(r10), "r"(r8),
+          "r"(r9),
+          // TODO: Fix this segfaulting with static_cast<void*>():
+          [a6] "re"(meta::bit_cast<void*>(arg6))
+        : "rcx", "r11", "memory", "cc");
     if (meta::bit_cast<i8>(result) < 0) {
         // The negative of `result` represents Linux's errno.
         return Failure(-meta::bit_cast<i8>(result));
@@ -56,42 +48,42 @@ auto syscall6(Any call, Any arg1, Any arg2, Any arg3, Any arg4, Any arg5,
 
 // TODO: Extract this to a .tpp file.
 template <typename ReturnType, typename T, typename... Args>
-auto syscall(T, Args... parameters) -> Result<ReturnType> {
-    constexpr i8 stdout = 1;
-    constexpr isize length = sizeof...(Args);
+auto syscall(T call, Args... parameters) -> Result<ReturnType>
+requires(sizeof...(Args) < 7) {
+    static constexpr isize length = sizeof...(Args);
     Any arguments[length] = {parameters...};
-    static_assert(length < 7,
-                  "Syscalls with more than 6 arguments are not supported!");
 
     using namespace std::detail;
     if constexpr (length == 0) {
-        syscall0(stdout);
+        syscall0(call);
         return okay;
     } else if constexpr (length == 1) {
-        syscall1(stdout, arguments[0]);
+        syscall1(call, arguments[0]);
         return okay;
     } else if constexpr (length == 2) {
-        syscall2(stdout, arguments[0], arguments[1]);
+        syscall2(call, arguments[0], arguments[1]);
         return okay;
     } else if constexpr (length == 3) {
-        syscall3(stdout, arguments[0], arguments[1], arguments[2]);
+        syscall3(call, arguments[0], arguments[1], arguments[2]);
         return okay;
     } else if constexpr (length == 4) {
-        syscall4(stdout, arguments[0], arguments[1], arguments[2],
-                 arguments[3]);
+        syscall4(call, arguments[0], arguments[1], arguments[2], arguments[3]);
         return okay;
     } else if constexpr (length == 5) {
-        syscall5(stdout, arguments[0], arguments[1], arguments[2], arguments[3],
+        syscall5(call, arguments[0], arguments[1], arguments[2], arguments[3],
                  arguments[4]);
         return okay;
     } else if constexpr (length == 6) {
-        return syscall6(stdout, arguments[0], arguments[1], arguments[2],
+        return syscall6(call, arguments[0], arguments[1], arguments[2],
                         arguments[3], arguments[4], arguments[5]);
     }
+    __builtin_unreachable();
 }
 
 // write() forwards its arguments to a failable stdout syscall.
 auto write(i8 const& file_descriptor, char8_t const* p_string_buffer,
            isize const& string_length) -> Result<> {
-    return syscall<void>(1, file_descriptor, p_string_buffer, string_length);
+    // return syscall<void>(1, file_descriptor, p_string_buffer, string_length);
+    return syscall<void>(1, file_descriptor, p_string_buffer, string_length, 0,
+                         0, 0);
 }
