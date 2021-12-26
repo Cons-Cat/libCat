83dd9a4cd9eb4c4770d9c502064e23744f72b9b8
Conscat <omg-lily2000@gmail.com>
2021-12-25 21:05:56 -0500

    Polished comments

--- src/runtime/start.h
+++ src/runtime/start.h
@@ -5,10 +5,11 @@
 #include <concepts.h>
 #include <type_traits.h>
 
-/* meow() is the user's entry point in a libCat program, rather than main().
- * This allows libCat to explore interesting ideas, such as enabling the user
- * to express explicit stack initialization, and giving them generally more
- * control over their program than the standard `crt0` or `crt1` do.
+/* `meow()` is the user's entry point in a libCat program, rather than `main()`.
+ * But hear meowt! This allows libCat to explore interesting ideas, such as
+ * enabling the user to express explicit stack initialization, and giving them
+ * generally more control over their program than the standard `crt0` or `crt1`
+ * do.
  *
  * That control is very generally useful. Do you need to support `.init` and
  * `.fini`, or do you only use `.ctor` or `.dtor` instead? Or both? Or neither?
@@ -17,17 +18,15 @@
  *
  * Does your program need to allocate a stack at all, or can you just use the
  * space you're already given in `argv[]`? Do you have to populate `argp[]`?
- * Does your program need to create a main pthread, or not? Should you align the
- * stack, or is that unnecessary? There are myriad creative liberties available
- * once you rethink basic assumptions about what C++ actually does.
- * libC is merely a suite of idioms, not our language semantics, so libCat
- * explores one alternative.
+ * Does your program need to create a main `pthread`, or not? Should you align
+ * the stack, or is that unnecessary? There are myriad creative liberties
+ * available once you rethink basic assumptions about what C++ actually does.
  *
- * As far as the name change from main() goes, GCC simply won't compile when it
- * thinks the main() definition is ill-formed, so this change *is* necessary
- * for fundamentally different behavior, such as not returning `int`. Meow is
- * four letters, it starts with 'm', and it sounds cute, so it's as good as
- * anything else. */
+ * As far as the name change from `main()` goes, GCC simply won't compile when
+ * it thinks the `main()` definition is ill-formed, so this change *is*
+ * necessary for fundamentally different behavior, such as not returning `int`.
+ * Meow is four letters, it starts with 'm', and it sounds cute, so it seems as
+ * good as anything else. */
 
 /* TODO: #ifdef __SANITIZE_ADDRESS__
  * to handle address sanitizers, which do not work. */
@@ -37,15 +36,17 @@
  * pushed to the stack, but otherwise all initialization logic shall occur in
  * meow().
  *
- * an _exit syscall is also not made in _start(), because GCC cannot inline
+ * an `_exit` syscall is also not made in `_start()`, because GCC cannot inline
  * `call` assembly instructions, and a more sophisticated exit strategy may be
- * desired by a user anyways. In libCat, program termination is explicit. */
+ * desired by a user anyways. For instance, stack unwinding or zero-ing out
+ * memory. In libCat, program termination is completely explicit. */
+
 extern "C" [[gnu::used]] void _start() {
     /* This cannot be simplified any further without producing unreliable
      * codegen. */
     asm(R"(mov %rsp, %rbp
            call meow)");
-    __builtin_unreachable();  // This elides a ret instruction.
+    __builtin_unreachable();  // This elides a `ret` instruction.
 }
 
 auto load_base_stack_pointer() -> void* {
@@ -78,8 +79,8 @@ void dont_optimize_out(auto& variable) {
     asm volatile("" ::"m"(variable));
 }
 
-/* __stack_chk_fail() is called when stack overflow occurs in programs compiled
- * without -fno-stack-protector. */
+/* `__stack_chk_fail()` is called when stack overflow occurs in programs
+ * compiled without `-fno-stack-protector`. */
 extern "C" void __stack_chk_fail() {
     exit(1);
 }
