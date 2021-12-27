84affaee1eef4ced69d711b248732a349e481998
Conscat <omg-lily2000@gmail.com>
2021-12-26 19:40:13 -0500

    Separated function implementations into `*.cpp` files.

--- src/runtime/exit.h
+++ src/runtime/exit.h
@@ -7,7 +7,4 @@
 // TODO: Use isize here:
 /* In libCat, the exit() function is provided globally instead of in <cstdlib>.
  * This streamlines out the existence of _exit(). */
-extern "C" [[gnu::used]] void exit(int exit_code) {
-    asm(R"(syscall)" : : "D"(meta::decay_numeral(exit_code)), "a"(60));
-    __builtin_unreachable();  // This elides a ret instruction.
-}
+extern "C" [[gnu::used]] void exit(int exit_code);
--- src/runtime/start.h
+++ src/runtime/start.h
@@ -41,46 +41,12 @@
  * desired by a user anyways. For instance, stack unwinding or zero-ing out
  * memory. In libCat, program termination is completely explicit. */
 
-extern "C" [[gnu::used]] void _start() {
-    /* This cannot be simplified any further without producing unreliable
-     * codegen. */
-    asm(R"(mov %rsp, %rbp
-           call meow)");
-    __builtin_unreachable();  // This elides a `ret` instruction.
-}
+extern "C" [[gnu::used]] void _start();
 
-auto load_base_stack_pointer() -> void* {
-    void* rbp;
-    asm("mov %%rbp, %[rbp]" : [rbp] "=r"(rbp));
-    return rbp;
-}
-
-auto load_argc() -> i4 {
-    i4 argc;
-    asm("mov 0(%%rbp), %[argc]" : [argc] "=r"(argc));
-    return argc;
-}
-
-auto load_argv() -> char8_t** {
-    char8_t** argv;
-    asm("lea 8(%%rbp), %[argv]" : [argv] "=r"(argv));
-    return argv;
-}
-
-void align_stack_pointer_16() {
-    asm("and $-16, %rsp");
-}
-
-void align_stack_pointer_32() {
-    asm("and $-32, %rsp");
-}
-
-void dont_optimize_out(auto& variable) {
-    asm volatile("" ::"m"(variable));
-}
-
-/* `__stack_chk_fail()` is called when stack overflow occurs in programs
- * compiled without `-fno-stack-protector`. */
-extern "C" void __stack_chk_fail() {
-    exit(1);
-}
+auto load_base_stack_pointer() -> void*;
+auto load_argc() -> i4;
+auto load_argv() -> char8_t**;
+void align_stack_pointer_16();
+void align_stack_pointer_32();
+void dont_optimize_out(auto& variable);
+extern "C" void __stack_chk_fail();
