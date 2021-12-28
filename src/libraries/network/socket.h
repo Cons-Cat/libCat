c56ab10aa6af108264822403808f5034ad9cb2f9
Conscat <wgooch2000@gmail.com>
2021-12-28 00:30:50 -0500

    Polished `socket.h`

--- src/libraries/network/socket.h
+++ src/libraries/network/socket.h
@@ -40,14 +40,16 @@ auto socket(i4 protocol_family, i4 type, i4 protocol)
     return syscall3(41u, protocol_family, type, protocol);
 }
 
-struct Socket {
+// TODO: Make this into a `concept`.
+struct SocketAddr {
     u4 socket_address_family;    // address family.
     char socket_address_data[];  // socket address (variable-length data)
 };
 
-auto connect(FileDescriptor connecting_socket, Socket* p_socket, i4 socket_size)
+template <typename SocketType>
+auto connect_to(FileDescriptor connecting_socket, SocketType* p_socket)
     -> Result<> {
-    return syscall3(42u, connecting_socket, p_socket, socket_size);
+    return syscall3(42u, connecting_socket, p_socket, sizeof(SocketType));
 }
 
 /* Returns a `FileDescriptor` for a socket that is requesting a connection, and
@@ -58,16 +60,31 @@ auto connect(FileDescriptor connecting_socket, Socket* p_socket, i4 socket_size)
  * binary layout as that socket which this expects to connect with. A
  * `FileDescriptor` representing the accepted socket is returned.
  *
- * This function is side-effectful. `p_socket` is assigned the address of an
- * accepted socket. */
-auto accept(FileDescriptor listening_socket, auto* p_accepted_socket,
-            i4 flags = 0) -> Result<FileDescriptor> {
+ * This function is side-effectful. `p_accepted_socket` is assigned the address
+ * of an accepted socket. */
+template <typename SocketType>
+auto accept_connection(FileDescriptor listening_socket,
+                       SocketType* p_accepted_socket, i4 flags = 0)
+    -> Result<FileDescriptor> {
+    // TODO: This seems to be blocked by atomic intrinsics.
     // TODO: Make an enum for `accept4()` Linux flags.
-    syscall4(288u, listening_socket, p_accepted_socket,
-             sizeof(decltype(*p_accepted_socket)), flags);
+    isize socket_size = sizeof(SocketType);
+    syscall4(288u, listening_socket, p_accepted_socket, &socket_size, flags);
     // TODO: Return file descriptor.
     // https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/net/socket.c#n1838
     // https://docs.huihoo.com/doxygen/linux/kernel/3.7/include_2linux_2file_8h_source.html
     // https://docs.huihoo.com/doxygen/linux/kernel/3.7/fs_2file_8c_source.html#l00773
+    return 0;
+}
+
+// Returns the number of characters sent to `destination_socket`.
+template <typename SocketType>
+auto send_buffer_to(FileDescriptor sending_socket, void const* p_message_buffer,
+                    usize buffer_length, i4 flags,
+                    SocketType const* p_destination_socket) -> Result<isize> {
+    // TODO: This seems to be blocked by atomic intrinsics.
+    // TODO: Implement this behavior.
+    // https://filippo.io/linux-syscall-table/
+    // https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/net/socket.c#n2005
     return 0;
 }
