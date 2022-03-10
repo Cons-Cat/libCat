0cb1a00f32b47c3f2121ff11d4c490ae7743467a
Conscat <omg-lily2000@gmail.com>
2022-03-09 19:17:14 -0500

    Created SIMD `compare_string()` function.

---
+++ src/libraries/string/implementations/strlen.cpp
@@ -0,0 +1,10 @@
+// -*- mode: c++ -*-
+// vim: set ft=cpp:
+#include <stdint.h>
+#include <string>
+
+[[deprecated(
+    "strlen() is deprecated! Use std::string_length<T>() instead.")]] auto
+strlen(char const* p_string) -> size_t {
+    return std::string_length<size_t>(p_string);
+}
