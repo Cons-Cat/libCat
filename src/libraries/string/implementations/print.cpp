1767358ec78d6838570a5a2719d1f24d62198bc8
Conscat <wgooch2000@gmail.com>
2022-03-11 07:03:23 -0500

    Created `std::print()` and `std::print_line()` functions.

---
+++ src/libraries/string/implementations/print.cpp
@@ -0,0 +1,11 @@
+#include <linux>
+#include <result>
+#include <string>
+
+auto std::print(char const* p_string) -> Result<> {
+    return write(1, p_string, std::string_length(p_string));
+}
+
+auto std::print(StringView const string) -> Result<> {
+    return write(1, string.data(), string.length());
+}
---
+++ src/libraries/string/implementations/print_line.cpp
@@ -0,0 +1,21 @@
+#include <linux>
+#include <result>
+#include <string>
+
+// TODO: Improve this performance with format strings when they are implemented.
+
+auto std::print_line(char const* p_string) -> Result<> {
+    auto result_1 = write(1, p_string, std::string_length(p_string));
+    if (!result_1.is_okay) {
+        return result_1.failure_code;
+    }
+    return write(1, "/n", 1);
+}
+
+auto std::print_line(StringView const string) -> Result<> {
+    auto result_1 = write(1, string.data(), string.length());
+    if (!result_1.is_okay) {
+        return result_1.failure_code;
+    }
+    return write(1, "/n", 1);
+}
