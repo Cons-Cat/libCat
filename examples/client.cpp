0f1c96ac8cad7493f97c26d46b9ad17b6605ba84
Conscat <omg-lily2000@gmail.com>
2022-03-11 08:46:32 -0500

    Made client/server examples basically working.

--- examples/client.cpp
+++ examples/client.cpp
@@ -1,6 +1,32 @@
+#include <buffer>
 #include <linux>
 #include <memory>
 #include <socket.h>
+#include <string>
+
+#include "linux_flags"
+#include "runtime"
 
 void meow() {
+    int4 argc = load_argc();
+    char** argv = load_argv();
+
+    SocketLocal socket;
+    char const* socket_name = "/tmp/temp.sock";
+
+    // `1` is a streaming socket.
+    FileDescriptor data_socket = create_socket_local(1, 0).or_panic();
+    std::copy_memory(socket_name, &socket.path_name,
+                     sizeof(socket.path_name) - 1);
+
+    connect_socket(data_socket, &socket).or_panic();
+
+    // Send all command line arguments to the server.
+    for (isize i = 1; i < argc; i++) {
+        send_buffer(data_socket, argv[i], std::string_length(argv[i]), 0)
+            .or_panic();
+    }
+
+    close(data_socket).or_panic();
+    exit();
 }
--- src/libraries/string/implementations/print.cpp
+++ src/libraries/string/implementations/print.cpp
@@ -6,6 +6,6 @@ auto std::print(char const* p_string) -> Result<> {
     return write(1, p_string, std::string_length(p_string));
 }
 
-auto std::print(StringView const string) -> Result<> {
+auto std::print(StringView const& string) -> Result<> {
     return write(1, string.p_data, string.length);
 }
