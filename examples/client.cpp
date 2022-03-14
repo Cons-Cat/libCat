dc13796b3ad94a32b8e7fc7239df741ebdf6532c
Conscat <omg-lily2000@gmail.com>
2022-03-14 00:04:31 -0400

    Updated client/server example to use new sockets API.

--- examples/client.cpp
+++ examples/client.cpp
@@ -8,22 +8,20 @@ void meow(int argc, char* argv[]) {
     SocketLocal<SocketType::stream> socket;
     char const* socket_name = "/tmp/temp.sock";
 
-    // `1` is a streaming socket.
-    FileDescriptor data_socket = nix::create_socket_local(1, 0).or_panic();
+    socket.create().or_panic();
     std::copy_memory(socket_name, &socket.path_name,
                      sizeof(socket.path_name) - 1);
 
-    nix::connect_socket(data_socket, &socket, socket.get_size()).or_panic();
+    socket.connect().or_panic();
 
     // Send all command line arguments to the server.
     for (isize i = 1; i < argc; i++) {
-        nix::send_buffer(data_socket, argv[i], std::string_length(argv[i]), 0)
-            .or_panic();
+        socket.send_buffer(argv[i], 0).or_panic();
         if (i < argc - 1) {
-            nix::send_buffer(data_socket, " ", 1, 0).or_panic();
+            socket.send_buffer(" ", 1).or_panic();
         }
     }
 
-    nix::close(data_socket).or_panic();
+    socket.close().or_panic();
     std::exit();
 }
