d6fc2cecfd9a61a2367ea7598cc9159085f386d1
Conscat <omg-lily2000@gmail.com>
2022-03-21 20:21:16 -0400

    Many improvements to Hello World.

--- examples/hello.cpp
+++ examples/hello.cpp
@@ -1,8 +1,7 @@
-#include <linux>
+#include <string>
 
 void meow() {
-    char const* p_str = "Hello, Conscat!\n";
-    // TODO: See if `std::print()` is zero-overhead.
-    nix::write(1, p_str, 16).or_panic();
+    StringView string = "Hello, Conscat!\n";
+    std::print(string).or_panic();
     std::exit();
 }
