26d1df32fa2822be692317c94816dd408b1a5c8c
Conscat <omg-lily2000@gmail.com>
2022-03-23 02:38:38 -0400

    Forgotten file for X11 API...

---
+++ examples/window.cpp
@@ -0,0 +1,9 @@
+#include <allocators>
+#include <x>
+
+void meow() {
+    PageAllocator allocator;
+    xwin::initiate_connection(allocator).or_panic(
+        "Failed to create an X context!");
+    std::exit();
+}
