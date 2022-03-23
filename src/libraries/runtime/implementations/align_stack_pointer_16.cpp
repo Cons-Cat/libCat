4989061ad12aadcdaea0c35ddbbd1e6b06518e5a
Conscat <wgooch2000@gmail.com>
2022-03-23 13:43:03 -0400

    Cleaned up `runtime` API.

--- src/libraries/runtime/implementations/align_stack_pointer_16.cpp
+++
@@ -1,9 +0,0 @@
-// -*- mode: c++ -*-
-// vim: set ft=cpp:
-#include <runtime>
-
-/* Advance the `%rsp` stack-pointer register to the nearest 16-byte aligned
- * address. */
-// [[gnu::always_inline]] inline void std::align_stack_pointer_16() {
-//     asm("and $-16, %rsp");
-// }
