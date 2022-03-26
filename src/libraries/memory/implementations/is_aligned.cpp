bc543ee3999c016a270117652978f7f0171de948
Conscat <omg-lily2000@gmail.com>
2022-03-26 11:50:32 -0400

    Simplified `std::address_of()`.

--- src/libraries/memory/implementations/is_aligned.cpp
+++ src/libraries/memory/implementations/is_aligned.cpp
@@ -2,7 +2,7 @@
 // vim: set ft=cpp:
 #include <memory>
 
-auto std::is_aligned(void const volatile* pointer, ssize byte_alignment)
+auto std::is_aligned(void const volatile* p_pointer, ssize byte_alignment)
     -> bool1 {
-    return (reinterpret_cast<usize>(pointer) % byte_alignment) == 0;
+    return (reinterpret_cast<usize>(p_pointer) % byte_alignment) == 0;
 }
