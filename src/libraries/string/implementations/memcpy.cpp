60785c502f7c0d46c9cdeead587f98f2e55d65eb
Conscat <wgooch2000@gmail.com>
2022-03-27 11:46:04 -0400

    Removed unused libC compatibility headers.

--- src/libraries/string/implementations/memcpy.cpp
+++ src/libraries/string/implementations/memcpy.cpp
@@ -3,7 +3,7 @@
 #include <memory>
 
 [[deprecated("memcpy() is deprecated! Use simd::copy_buffer() instead!")]] auto
-memcpy(void* p_destination, void const* p_source, size_t bytes) -> void* {
+memcpy(void* p_destination, void const* p_source, usize bytes) -> void* {
     cat::copy_memory(p_source, p_destination, static_cast<ssize>(bytes));
     return p_destination;
 }
