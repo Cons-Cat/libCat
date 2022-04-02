d15bdd7614ef6a2cf08ab197930a57990e4f5ae8
Conscat <omg-lily2000@gmail.com>
2022-04-02 17:30:13 -0400

    Fix memcpy libC example.

--- examples/memory_copy_libc.cpp
+++ examples/memory_copy_libc.cpp
@@ -1,10 +1,10 @@
 #include <array>
-#include <ccatlib>
+#include <cstdlib>
 #include <cstring>
 
 auto main() -> int32_t {
-    cat::array<int32_t, 200000> source_2000;
-    cat::array<int32_t, 200000> dest_2000;
+    std::array<int32_t, 200000> source_2000;
+    std::array<int32_t, 200000> dest_2000;
     memcpy(&dest_2000, &source_2000, sizeof(dest_2000));
     // Prevent these from being optimized out.
     asm volatile("" ::"m"(source_2000));
