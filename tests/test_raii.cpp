6c4b62e9c6fb28870fb97018eb3f406737d47e53
Conscat <wgooch2000@gmail.com>
2022-04-02 15:52:50 -0400

    Refactored `Raii`.

--- tests/test_raii.cpp
+++ tests/test_raii.cpp
@@ -1,24 +1,59 @@
 #include <linux>
-#include <memory>
+#include <raii>
 #include <utility>
 
+int4 global = 0;
+
 struct Foo {
-    Foo() {
-        cat::print("Foo()\n").or_panic();
+    String data;
+
+    Foo(String string) : data(std::move(string)) {
+        cat::print(data).discard_result();
+        cat::print_line(" constructor").discard_result();
     }
+
     ~Foo() {
-        cat::print("~Foo()\n").or_panic();
+        cat::print("~").discard_result();
+        cat::print_line(this->data).discard_result();
+    }
+
+    auto operator=(String string) {
+        this->data = string;
+        return *this;
+    }
+
+    void raii() const {
+        cat::print(this->data).discard_result();
+        cat::print_line(" calls raii()!").discard_result();
+        global++;
     }
 };
 
-void func(Raii<Foo>) {
-}
+void func(Raii<Foo>){};
 
 void meow() {
-    // Call `Foo()` constructor:
-    Raii<Foo> foo;
-    /* Call `Foo()` constructor, then `Raii()` move-constructor, then `Foo()`
-     * destructor: */
+    cat::print_line("Construct objects.").discard_result();
+    // Test constructor.
+    Raii<Foo> foo(String("foo"));
+    // Test assignment.
+    foo = "foo";
+
+    Raii<Foo> moo(String("moo"));
+
+    // Test move-assignment.
+    cat::print_line("Move moo into foo.").discard_result();
+    foo = cat::move(moo);
+
+    cat::print_line("Move foo into func().").discard_result();
+    // `cat::move()` is required:
     func(cat::move(foo));
+
+    // This is correctly ill-formed:
+    // func(foo);
+
+    cat::print_line("Everything falls out of scope.").discard_result();
+
+    // `raii()` should have been called exactly twice.
+    Result(global == 2).or_panic();
     cat::exit();
 }
