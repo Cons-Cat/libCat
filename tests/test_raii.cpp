d253592ce4778a251c809b11c945c09603da1dc7
Conscat <wgooch2000@gmail.com>
2022-04-02 16:19:57 -0400

    Improvements to `Raii` ownership.

--- tests/test_raii.cpp
+++ tests/test_raii.cpp
@@ -1,11 +1,13 @@
-#include <linux>
 #include <raii>
+#include <string>
 #include <utility>
 
 int4 global = 0;
 
 struct Foo {
     String data;
+
+    Foo() = default;
 
     Foo(String string) : data(std::move(string)) {
         cat::print(data).discard_result();
@@ -18,7 +20,7 @@ struct Foo {
     }
 
     auto operator=(String string) {
-        this->data = string;
+        this->data = std::move(string);
         return *this;
     }
 
@@ -37,21 +39,31 @@ void meow() {
     Raii<Foo> foo(String("foo"));
     // Test assignment.
     foo = "foo";
+    Result(foo.has_ownership()).or_panic();
 
     Raii<Foo> moo(String("moo"));
+    Result(moo.has_ownership()).or_panic();
 
     // Test move-assignment.
     cat::print_line("Move moo into foo.").discard_result();
     foo = cat::move(moo);
+    Result(!moo.has_ownership()).or_panic();
 
     cat::print_line("Move foo into func().").discard_result();
     // `cat::move()` is required:
     func(cat::move(foo));
+    Result(!foo.has_ownership()).or_panic();
 
     // This is correctly ill-formed:
     // func(foo);
 
     cat::print_line("Everything falls out of scope.").discard_result();
+
+    // Default construct `Raii<Foo>`.
+    Raii<Foo> goo;
+    // Extract goo.
+    _ = goo.get();
+    Result(!goo.has_ownership()).or_panic();
 
     // `raii()` should have been called exactly twice.
     Result(global == 2).or_panic();
