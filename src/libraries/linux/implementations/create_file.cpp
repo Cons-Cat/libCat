cd5e51fc8b0ddae94bddb8ccae9ebd8fd6346c4d
Conscat <wgooch2000@gmail.com>
2022-04-03 17:24:47 -0400

    Created `nix::open_file()` and `nix::create_file()`.

---
+++ src/libraries/linux/implementations/create_file.cpp
@@ -0,0 +1,10 @@
+// -*- mode: c++ -*-
+// vim: set ft=cpp:
+#include <linux>
+
+auto nix::create_file(char const* p_file_path, nix::OpenMode file_mode)
+    -> Result<FileDescriptor> {
+    return nix::open_file(p_file_path, file_mode,
+                          nix::OpenFlags::create | nix::OpenFlags::truncate |
+                              nix::OpenMode::read_write);
+}
