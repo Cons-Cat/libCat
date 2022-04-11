95a650d9e2229b70a975d98c44dde84f8d823b1a
Conscat <wgooch2000@gmail.com>
2022-04-10 20:24:20 -0400

    Fixed SIMD alignment, and added SIMD unit tests.

--- src/libraries/memory/implementations/copy_memory.cpp
+++ src/libraries/memory/implementations/copy_memory.cpp
@@ -6,9 +6,6 @@
 // TODO: Make integers consistently signed.
 /* Copy some bytes from one address to another address. */
 void cat::copy_memory(void const* p_source, void* p_destination, ssize bytes) {
-    // `Vector` is the width of a 32-byte AVX register.
-    // `long long int` is required for some SIMD intrinsics.
-    // using Vector = cat::detail::Simd<long long int, 4, 4>;
     using Vector = int8x_;
 
     unsigned char const* p_source_handle =
@@ -18,36 +15,38 @@ void cat::copy_memory(void const* p_source, void* p_destination, ssize bytes) {
     constexpr ssize l3_cache_size = 2_mi;
     ssize padding;
 
-    if (bytes <= 256) {
+    constexpr ssize step_size = sizeof(Vector) * 8;
+
+    if (bytes <= step_size) {
         cat::copy_memory_small(p_source, p_destination, bytes);
+        return;
     }
 
     // Align source, destination, and bytes to the vector's optimal alignment.
     padding = static_cast<ssize>(
-        (sizeof(Vector) - ((meta::bit_cast<usize>(p_destination_handle)) &
-                           (sizeof(Vector) - 1))) &
-        (sizeof(Vector) - 1));
+        (alignof(Vector) - ((meta::bit_cast<usize>(p_destination_handle)) &
+                            (alignof(Vector) - 1))) &
+        (alignof(Vector) - 1));
 
-    Vector head = *meta::bit_cast<Vector const*>(p_source_handle);
-    *static_cast<Vector*>(p_destination) = head;
+    cat::copy_memory_small(p_source, p_destination, padding);
 
     p_source_handle += padding;
     p_destination_handle += padding;
     bytes -= padding;
-
     Vector vectors[8];
-    constexpr ssize step_size = sizeof(Vector) * 8;
+
     // This routine is optimized for buffers in L3 cache. Streaming is
-    // slower.
+    // slower there.
     if (bytes <= l3_cache_size) {
         while (bytes >= step_size) {
-            /* Load 8 8x4 vectors, then increment the source pointer by that
+            /* Load 8 vectors, then increment the source pointer by that
              * size. */
 #pragma GCC unroll 8
             for (int4 i = 0; i < 8; i++) {
-                vectors[i] = meta::bit_cast<Vector*>(p_source_handle)[i];
+                vectors[i] = meta::bit_cast<Vector const*>(p_source_handle)[i];
             }
-            simd::prefetch_for_one_read(p_source_handle + 512);
+            simd::prefetch_for_one_read(p_source_handle + (step_size * 2));
+
 #pragma GCC unroll 8
             for (int4 i = 0; i < 8; i++) {
                 meta::bit_cast<Vector*>(p_destination_handle)[i] = vectors[i];
@@ -57,9 +56,11 @@ void cat::copy_memory(void const* p_source, void* p_destination, ssize bytes) {
             bytes -= step_size;
         }
     }
+
     // This routine is run when the memory source cannot fit in cache.
     else {
         simd::prefetch_for_one_read(p_source_handle + 512);
+        // TODO: This code block has fallen far out of date.
         /* TODO: This could be improved by using aligned-streaming when
          * possible. */
         while (bytes >= 256) {
@@ -76,6 +77,7 @@ void cat::copy_memory(void const* p_source, void* p_destination, ssize bytes) {
             p_destination_handle += 256;
             bytes -= 256;
         }
+
         simd::sfence();
     }
 
