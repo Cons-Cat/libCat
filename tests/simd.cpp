e1aa16f1b37eab018a5e1b3a5b59c54992ea681d
Conscat <omg-lily2000@gmail.com>
2022-04-10 20:24:20 -0400

    Fixed SIMD alignment, and added SIMD unit tests.

---
+++ tests/simd.cpp
@@ -0,0 +1,11 @@
+void meow() {
+    // Test that vector arithmetic does not segfault.
+    int4x4 vec1 = {0, 1, 2, 3};
+    int4x4 vec2{0, 1, 2, 3};
+    vec1 += vec2;
+    _ = vec1 + vec2;
+
+    // TODO: Test correctness of vector operations.
+
+    cat::exit();
+}
