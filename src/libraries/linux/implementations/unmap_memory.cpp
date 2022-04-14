c8d2fdf5b8c0413844d91b27a59f311be720f969
Conscat <wgooch2000@gmail.com>
2022-04-14 15:38:43 -0400

    Refactored `PageAllocator` and `Result` to facilitate `.free()`.

--- src/libraries/linux/implementations/unmap_memory.cpp
+++ src/libraries/linux/implementations/unmap_memory.cpp
@@ -2,7 +2,7 @@
 // vim: set ft=cpp:
 #include <linux>
 
-// TODO: `unmap_memory()` wraps the `munmap` Linux syscall.
-auto nix::unmap_memory() -> Result<> {
-    return okay;
+// `nix::unmap_memory()` wraps the `munmap` Linux syscall.
+auto nix::unmap_memory(void* p_memory, ssize length) -> Result<> {
+    return nix::syscall2(11, p_memory, length);
 }
