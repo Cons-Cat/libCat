eceece1cc33ad0daca5c73ce6cf4989e5b7b578b
Conscat <omg-lily2000@gmail.com>
2022-04-14 15:38:43 -0400

    Refactored `PageAllocator` and `Result` to facilitate `.free()`.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -1,13 +1,17 @@
 #include <allocators>
+#include <array>
 
 void meow() {
     // Initialize an allocator.
     PageAllocator allocator;
     // Allocate a page.
-    allocator.malloc(32).or_panic();
+    PageMemoryHandle memory = allocator.malloc(32).or_panic();
     // Write to the page.
-    allocator[0] = 10;
-    Result(allocator[0] == 10).or_panic();
+    auto& p_int = *static_cast<int4*>(memory.as_address());
+    p_int = 10;
+    Result(p_int == 10).or_panic();
+    // Free the page.
+    allocator.free(memory).or_panic();
 
     cat::exit();
 };
