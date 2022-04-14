83ae2bdd6ce943fb1d8630762ba30a5f191e48bd
Conscat <wgooch2000@gmail.com>
2022-04-14 15:42:35 -0400

    Reorganized allocator handles.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -5,7 +5,7 @@ void meow() {
     // Initialize an allocator.
     PageAllocator allocator;
     // Allocate a page.
-    PageMemoryHandle memory = allocator.malloc(32).or_panic();
+    auto memory = allocator.malloc(32).or_panic();
     // Write to the page.
     auto& p_int = *static_cast<int4*>(memory.as_address());
     p_int = 10;
