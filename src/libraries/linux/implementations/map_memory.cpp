7ec7067f7567628899cc872dec17753743dd7b69
Conscat <omg-lily2000@gmail.com>
2022-04-15 01:41:29 -0400

    Simplified mmap flag enum names.

--- src/libraries/linux/implementations/map_memory.cpp
+++ src/libraries/linux/implementations/map_memory.cpp
@@ -5,7 +5,7 @@
 // `map_memory()` wraps the `mmap` Linux syscall. This returns the virtual
 // memory address which it has allocated a page at.
 auto nix::map_memory(usize beginning_address, usize bytes_size,
-                     MmapProtectionFlags protections, MmapMemoryFlags flags,
+                     MemoryProtectionFlags protections, MemoryFlags flags,
                      FileDescriptor file_descriptor, ssize pages_offset)
     -> Result<void*> {
     return nix::syscall6(9u, beginning_address, bytes_size, protections, flags,
