d9324bc2e338fe5cf9cb121c6044d43737edbfd1
Conscat <wgooch2000@gmail.com>
2022-04-15 01:41:29 -0400

    Simplified mmap flag enum names.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -7,8 +7,7 @@ void meow() {
     // Allocate a page.
     auto memory = allocator.malloc<int4>().or_panic();
     // Write to the page.
-    int4& foo = memory.get();
-    foo = 10;
+    memory.get() = 10;
     Result(*static_cast<int4*>(memory.as_address()) == 10).assert();
     // Free the page.
     allocator.free(memory).or_panic();
