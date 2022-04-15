6fa74497575332043a680bb564b8f4a70d03e7f6
Conscat <omg-lily2000@gmail.com>
2022-04-15 18:10:37 -0400

    Freed all memory allocations.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -5,7 +5,16 @@ void meow() {
     // Initialize an allocator.
     cat::PageAllocator allocator;
     // Allocate a page.
-    auto memory = allocator.malloc<int4>().value();
+    bool1 failed = false;
+    auto memory = allocator.malloc<int4>()
+                      .or_else([&]() {
+                          failed = true;
+                      })
+                      .value();
+    if (failed) {
+        cat::exit(1);
+    }
+
     // Write to the page.
     allocator.get(memory) = 10;
     Result(allocator.get(memory) == 10).assert();
