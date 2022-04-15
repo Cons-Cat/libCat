fdc38c4d63d67e7328d134f9deb31bc6358b58b8
Conscat <omg-lily2000@gmail.com>
2022-04-15 16:03:19 -0400

    Improved dereferencing memory handles.

--- src/libraries/linux/implementations/map_memory.cpp
+++ src/libraries/linux/implementations/map_memory.cpp
@@ -4,10 +4,11 @@
 
 // `map_memory()` wraps the `mmap` Linux syscall. This returns the virtual
 // memory address which it has allocated a page at.
-auto nix::map_memory(usize beginning_address, usize bytes_size,
-                     MemoryProtectionFlags protections, MemoryFlags flags,
-                     FileDescriptor file_descriptor, ssize pages_offset)
-    -> Result<void*> {
+auto nix::map_memory(usize const beginning_address, usize const bytes_size,
+                     MemoryProtectionFlags const protections,
+                     MemoryFlags const flags,
+                     FileDescriptor const file_descriptor,
+                     ssize const pages_offset) -> Result<void*> {
     return nix::syscall6(9u, beginning_address, bytes_size, protections, flags,
                          file_descriptor, pages_offset * page_size);
 }
--- src/libraries/linux/implementations/unmap_memory.cpp
+++ src/libraries/linux/implementations/unmap_memory.cpp
@@ -3,6 +3,7 @@
 #include <linux>
 
 // `nix::unmap_memory()` wraps the `munmap` Linux syscall.
-auto nix::unmap_memory(void* p_memory, ssize length) -> Result<> {
+auto nix::unmap_memory(void const* const p_memory, ssize const length)
+    -> Result<> {
     return nix::syscall2(11, p_memory, length);
 }
