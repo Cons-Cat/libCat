261979ea1e286fd3af16dd6f0e4c9000da3f5d99
Conscat <wgooch2000@gmail.com>
2022-04-15 19:31:41 -0400

    Supported small-size optimization for `cat::PageAllocator`.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -1,12 +1,13 @@
 #include <allocators>
 #include <array>
+#include <math>
 
 void meow() {
     // Initialize an allocator.
     cat::PageAllocator allocator;
     // Allocate a page.
     bool1 failed = false;
-    auto memory = allocator.malloc<int4>()
+    auto memory = allocator.malloc<int4>(1_ki)
                       .or_else([&]() {
                           failed = true;
                       })
@@ -17,9 +18,29 @@ void meow() {
 
     // Write to the page.
     allocator.get(memory) = 10;
-    Result(allocator.get(memory) == 10).assert();
+    Result(allocator.get(memory) == 10).or_panic();
     // Free the page.
     allocator.free(memory).or_panic();
 
+    // Allocation with small-size optimization.
+    int stack_variable;
+    auto small_memory =
+        allocator.malloca<int4>().value();
+    allocator.get(small_memory) = 2;
+    // Both values should be on stack, so these addresses are close
+    // together.
+    Result(cat::abs(intptr{&stack_variable} -
+                    intptr{&allocator.get(small_memory)}) < 512)
+        .or_panic();
+    allocator.freea(small_memory).discard_result();
+
+	small_memory = allocator.malloca<int4>(1_ki).value();
+    // `small_memory` should be in a page, so these addresses are far
+    // apart.
+    Result(cat::abs(intptr{&stack_variable} -
+                    intptr{&allocator.get(small_memory)}) > 512)
+        .or_panic();
+	allocator.freea(small_memory).discard_result();
+	
     cat::exit();
 };
