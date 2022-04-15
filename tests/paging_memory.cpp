25cbd396ac96c74ecdc74ebece4e67efbe696cd3
Conscat <wgooch2000@gmail.com>
2022-04-15 16:41:27 -0400

    Removed getter methods from memory handles.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -8,7 +8,7 @@ void meow() {
     auto memory = allocator.malloc<int4>().or_panic();
     // Write to the page.
     allocator.get(memory) = 10;
-    Result(*static_cast<int4*>(memory.as_address()) == 10).assert();
+    Result(allocator.get(memory) == 10).assert();
     // Free the page.
     allocator.free(memory).or_panic();
 
