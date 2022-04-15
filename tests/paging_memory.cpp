61acf593b4ad031649579b869934f01668d6bb00
Conscat <omg-lily2000@gmail.com>
2022-04-15 17:35:48 -0400

    Made `.malloc()` return an `Optional` instead of a `Result`.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -5,7 +5,7 @@ void meow() {
     // Initialize an allocator.
     cat::PageAllocator allocator;
     // Allocate a page.
-    auto memory = allocator.malloc<int4>().or_panic();
+    auto memory = allocator.malloc<int4>().value();
     // Write to the page.
     allocator.get(memory) = 10;
     Result(allocator.get(memory) == 10).assert();
