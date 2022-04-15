91192b9fcbd6aabf2fcd4e0695f1adfcccd5d93f
Conscat <omg-lily2000@gmail.com>
2022-04-15 01:24:35 -0400

    Made memory handle carry its type.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -7,7 +7,7 @@ void meow() {
     // Allocate a page.
     auto memory = allocator.malloc<int4>().or_panic();
     // Write to the page.
-    int4& foo = *static_cast<int4*>(memory.as_address());
+    int4& foo = memory.get();
     foo = 10;
     Result(*static_cast<int4*>(memory.as_address()) == 10).assert();
     // Free the page.
