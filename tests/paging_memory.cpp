6dee26e755fe37af84ec958972823fcc3aa34cf9
Conscat <omg-lily2000@gmail.com>
2022-04-15 01:08:45 -0400

    Supported typed `malloc()`.

--- tests/paging_memory.cpp
+++ tests/paging_memory.cpp
@@ -5,11 +5,11 @@ void meow() {
     // Initialize an allocator.
     PageAllocator allocator;
     // Allocate a page.
-    auto memory = allocator.malloc(32).or_panic();
+    auto memory = allocator.malloc<int4>().or_panic();
     // Write to the page.
-    auto& p_int = *static_cast<int4*>(memory.as_address());
-    p_int = 10;
-    Result(p_int == 10).or_panic();
+    int4& foo = *static_cast<int4*>(memory.as_address());
+    foo = 10;
+    Result(*static_cast<int4*>(memory.as_address()) == 10).assert();
     // Free the page.
     allocator.free(memory).or_panic();
 
