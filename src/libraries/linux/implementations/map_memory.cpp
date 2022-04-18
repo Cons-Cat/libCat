e5fcf61ebeb7178069fb9f7febb2ee1e83e80629
Conscat <wgooch2000@gmail.com>
2022-04-18 13:39:40 -0400

    Improved alignment utilities.

--- src/libraries/linux/implementations/map_memory.cpp
+++ src/libraries/linux/implementations/map_memory.cpp
@@ -9,6 +9,7 @@ auto nix::map_memory(usize const beginning_address, usize const bytes_size,
                      MemoryFlags const flags,
                      FileDescriptor const file_descriptor,
                      ssize const pages_offset) -> Result<void*> {
+    // TODO: Consider `__builtin_assume_aligned()`.
     return nix::syscall6(9u, beginning_address, bytes_size, protections, flags,
                          file_descriptor, pages_offset * page_size);
 }
