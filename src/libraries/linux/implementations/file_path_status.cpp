f36d91d6d2dad9a78955fa18db82cbca8ddc7538
Conscat <omg-lily2000@gmail.com>
2022-05-16 18:35:30 -0400

    Minor clean up.

--- src/libraries/linux/implementations/file_path_status.cpp
+++ src/libraries/linux/implementations/file_path_status.cpp
@@ -5,7 +5,8 @@
 auto nix::file_path_status(cat::String const& file_path)
     -> cat::Scaredy<nix::FileStatus, nix::LinuxError> {
     nix::FileStatus status;
-    cat::Scaredy result = nix::syscall<void>(4, file_path.p_data(), &status);
+    nix::ScaredyLinux<void> result =
+        nix::syscall<void>(4, file_path.p_data(), &status);
     if (result.has_value()) {
         return status;
     }
