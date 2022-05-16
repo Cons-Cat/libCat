308a7cabf210bb9ed3f8ed950ebd7d087bf69a70
Conscat <wgooch2000@gmail.com>
2022-05-16 18:35:30 -0400

    Minor clean up.

--- src/libraries/linux/implementations/file_descriptor_status.cpp
+++ src/libraries/linux/implementations/file_descriptor_status.cpp
@@ -5,7 +5,8 @@
 auto nix::file_descriptor_status(nix::FileDescriptor const file_descriptor)
     -> cat::Scaredy<nix::FileStatus, nix::LinuxError> {
     nix::FileStatus status;
-    cat::Scaredy result = nix::syscall<void>(5, file_descriptor, &status);
+    nix::ScaredyLinux<void> result =
+        nix::syscall<void>(5, file_descriptor, &status);
     if (result.has_value()) {
         return status;
     }
