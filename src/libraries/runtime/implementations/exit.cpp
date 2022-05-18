1c8a165e282e35ee09cf6f7532b656395f9b36ce
Conscat <wgooch2000@gmail.com>
2022-05-18 00:57:26 -0400

    Moved `cat::detail::Numeral` to `cat::Numeral`.

--- src/libraries/runtime/implementations/exit.cpp
+++ src/libraries/runtime/implementations/exit.cpp
@@ -5,7 +5,6 @@
 // Terminate the program. Without arguments, this exits with a success code for
 // the target operating system.
 extern "C" [[gnu::used]] void cat::exit(ssize exit_code) {
-    // TODO: is `meta::decay_numeral()` needed here? Why?
     asm("syscall" : : "D"(exit_code), "a"(60));
     __builtin_unreachable();  // This elides a `ret` instruction.
 }
