afd3d20ab141b6a26b43965022118d3f2eff042d
Conscat <omg-lily2000@gmail.com>
2022-05-23 12:39:04 -0400

    Added `cat::ForwardList`.

--- tests/test_list.cpp
+++ tests/test_list.cpp
@@ -9,9 +9,9 @@ void meow() {
 
     // Test insert.
     cat::List<int4> list_1;
-    _ = list_1.insert(allocator, list_1.begin(), 3);
-    _ = list_1.insert(allocator, list_1.begin(), 2);
-    _ = list_1.insert(allocator, list_1.begin(), 1);
+    list_1.insert(allocator, list_1.begin(), 3).or_panic();
+    list_1.insert(allocator, list_1.begin(), 2).or_panic();
+    list_1.insert(allocator, list_1.begin(), 1).or_panic();
     Result(list_1.front() == 1).or_panic();
     Result(list_1.back() == 3).or_panic();
 
@@ -27,8 +27,8 @@ void meow() {
 
     // Test push.
     cat::List<int4> list_2;
-    _ = list_2.push_front(allocator, 0);
-    _ = list_2.push_back(allocator, 4);
+    _ = list_2.push_front(allocator, 0).or_panic();
+    _ = list_2.push_back(allocator, 4).or_panic();
     Result(list_2.front() == 0).or_panic();
     Result(list_2.back() == 4).or_panic();
     _ = list_2.insert(allocator, ++list_2.begin(), 1);
@@ -42,9 +42,9 @@ void meow() {
 
     // Test emplace.
     cat::List<int4> list_3;
-    _ = list_3.emplace_front(allocator, 1);
-    _ = list_3.emplace_front(allocator, 2);
-    _ = list_3.emplace_back(allocator, 3);
+    _ = list_3.emplace_front(allocator, 1).or_panic();
+    _ = list_3.emplace_front(allocator, 2).or_panic();
+    _ = list_3.emplace_back(allocator, 3).or_panic();
     _ = list_3.emplace(allocator, ++list_3.begin(), 4);
     Result(list_3.front() == 2).or_panic();
     Result(list_3.back() == 3).or_panic();
@@ -74,6 +74,26 @@ void meow() {
     }
 
     list_2.clear(allocator);
+
+    // Test `ForwardList`.
+    allocator.reset();
+    cat::ForwardList<int4> forward_list_1;
+    _ = forward_list_1.push_front(allocator, 0).or_panic();
+    _ = forward_list_1.emplace_front(allocator, 1).or_panic();
+    forward_list_1.insert_after(allocator, forward_list_1.begin() + 1, 2)
+        .or_panic();
+    forward_list_1.emplace_after(allocator, forward_list_1.end(), 3).or_panic();
+
+    Result(*forward_list_1.begin() == 1).or_panic();
+    Result(*(forward_list_1.begin() + 1) == 0).or_panic();
+    Result(*(forward_list_1.begin() + 2) == 2).or_panic();
+    Result(*(forward_list_1.begin() + 3) == 3).or_panic();
+
+    forward_list_1.erase_after(allocator, forward_list_1.begin());
+    Result(*(forward_list_1.begin() + 1) == 2).or_panic();
+
+    forward_list_1.pop_front(allocator);
+    Result(*forward_list_1.begin() == 2).or_panic();
 
     _ = page_allocator.free(page);
     cat::exit();
