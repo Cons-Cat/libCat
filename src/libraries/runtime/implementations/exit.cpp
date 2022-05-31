1c940035cfbea2e44fbc361b67a8d3c49e061648
Conscat <omg-lily2000@gmail.com>
2022-05-31 00:07:20 -0400

    Many minor improvements.

--- src/libraries/runtime/implementations/exit.cpp
+++ src/libraries/runtime/implementations/exit.cpp
@@ -2,7 +2,7 @@
 
 // Terminate the program. Without arguments, this exits with a success code for
 // the target operating system.
-extern "C" [[gnu::used]] void cat::exit(ssize exit_code) {
+[[gnu::used, noreturn]] void cat::exit(ssize exit_code) {
     asm("syscall" : : "D"(exit_code), "a"(60));
     __builtin_unreachable();  // This elides a `ret` instruction.
 }
