6bb4e2993aac61cf646a89c3137679e542466429
Conscat <wgooch2000@gmail.com>
2022-06-04 17:28:48 -0400

    Reorganized SIMD ABI implementations.

---
+++ src/libraries/simd/cat/detail/simd_avx2.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <cat/detail/simd_avx2_fwd.hpp>
+
+#include <cat/meta>
+#include <cat/numerals>
+#include <cat/simd>
+
+namespace cat {
+
+template <typename T>
+[[nodiscard]] auto testc(SimdMask<Avx2Abi<T>, T> const left,
+                         SimdMask<Avx2Abi<T>, T> const right) -> int4 {
+    if constexpr (is_same<T, float>) {
+        return __builtin_ia32_vtestcps256(left.raw, right.raw);
+    } else if constexpr (is_same<T, double>) {
+        return __builtin_ia32_vtestcpd256(left.raw, right.raw);
+    } else {
+        auto left_raw =
+            reinterpret_cast<Avx2Simd<long long int>::Raw>(left.raw);
+        auto right_raw =
+            reinterpret_cast<Avx2Simd<long long int>::Raw>(right.raw);
+        return __builtin_ia32_ptestc256(left_raw, right_raw);
+    }
+}
+
+template <typename T>
+[[nodiscard]] auto testz(SimdMask<Avx2Abi<T>, T> const left,
+                         SimdMask<Avx2Abi<T>, T> const right) -> int4 {
+    if constexpr (is_same<T, float>) {
+        return __builtin_ia32_vtestzps256(left.raw, right.raw);
+    } else if constexpr (is_same<T, double>) {
+        return __builtin_ia32_vtestzpd256(left.raw, right.raw);
+    } else {
+        auto left_raw =
+            reinterpret_cast<Avx2Simd<long long int>::Raw>(left.raw);
+        auto right_raw =
+            reinterpret_cast<Avx2Simd<long long int>::Raw>(right.raw);
+        return __builtin_ia32_ptestz256(left_raw, right_raw);
+    }
+}
+
+// Implementation of `all_of()` for AVX2.
+template <typename T>
+[[nodiscard]] auto all_of(SimdMask<Avx2Abi<T>, T> const mask) -> bool {
+    return testc(mask, mask == mask) != 0;
+}
+
+// Implementation of `any_of()` for AVX2.
+template <typename T>
+[[nodiscard]] auto any_of(SimdMask<Avx2Abi<T>, T> const mask) -> bool {
+    return testz(mask, mask == mask) == 0;
+}
+
+// TODO: Return a `Bitset`.
+// Implementation of `move_mask` for AVX2.
+template <typename T>
+[[nodiscard]] auto move_mask(Avx2Simd<T> const vector) -> int4 {
+    if constexpr (is_same<T, float>) {
+        // Create a bitmask from the most significant bit of every `float` in
+        // this vector.
+        return __builtin_ia32_movmskps256(vector.raw);
+    } else if constexpr (is_same<T, double>) {
+        // Create a bitmask from the most significant bit of every `double` in
+        // this vector.
+        return __builtin_ia32_movmskpd256(vector.raw);
+    } else {
+        // Create a bitmask from the most significant bit of every byte in this
+        // vector.
+        return __builtin_ia32_pmovmskb256(vector.raw);
+    }
+}
+
+// TODO: Return a `Bitset`.
+// Implementation of `move_mask` for AVX2.
+template <typename T>
+[[nodiscard]] auto move_mask(SimdMask<Avx2Abi<T>, T> const mask) -> int4 {
+    if constexpr (is_same<T, float>) {
+        // Create a bitmask from the most significant bit of every `float` in
+        // this vector.
+        return __builtin_ia32_movmskps256(mask.raw);
+    } else if constexpr (is_same<T, double>) {
+        // Create a bitmask from the most significant bit of every `double` in
+        // this vector.
+        return __builtin_ia32_movmskpd256(mask.raw);
+    } else {
+        // Create a bitmask from the most significant bit of every byte in this
+        // vector.
+        return __builtin_ia32_pmovmskb256(mask.raw);
+    }
+}
+
+}  // namespace cat
---
+++ src/libraries/simd/cat/detail/simd_sse42_fwd.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace cat {
+
+// Forward declarations.
+template <typename Abi, typename T>
+requires(is_same<typename Abi::Scalar, T>) class alignas(
+    Abi::alignment.raw) Simd;
+
+template <typename Abi, typename T>
+class alignas(Abi::alignment.raw) SimdMask;
+
+// `Sse2Abi` is a SIMD ABI that can be expected to work on any x86-64 build
+// target.
+template <typename T>
+struct Sse42Abi {
+    using Scalar = T;
+
+    // Produce a similar `Sse42Abi` for type `U`.
+    template <typename U>
+    using MakeAbi = Sse42Abi<U>;
+
+    Sse42Abi() = delete;
+
+    static constexpr ssize size = 16;
+    static constexpr ssize lanes = size / ssizeof<T>();
+    static constexpr ssize alignment = 16;
+};
+
+template <typename T>
+using Sse42Simd = Simd<Sse42Abi<T>, T>;
+
+template <typename T>
+using Sse42SimdMask = SimdMask<Sse42Abi<T>, T>;
+
+}  // namespace cat
