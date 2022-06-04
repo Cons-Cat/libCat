d439ec1f87a32ad4fe0f91479718ac45d1cce874
Conscat <omg-lily2000@gmail.com>
2022-06-04 18:34:13 -0400

    Many various minor improvements.

--- examples/echo.cpp
+++ examples/echo.cpp
@@ -1,13 +1,16 @@
 #include <cat/linux>
 
+// TODO: This seems to be broken in -O0, but not -03.
 // TODO: Simplify with `cat::Span`.
 // TODO: Make this cross-platform.
 void meow(int argc, char* p_argv[]) {
     for (int4 i = 1; i < argc; i++) {
         int4 length = 0;
+        // TODO: Use a `string_length_scalar()`.
         while (p_argv[i.raw][length.raw] != 0) {
             length++;
         }
+
         p_argv[i.raw][length.raw] = ' ';
         _ = nix::write(nix::FileDescriptor{1}, p_argv[i.raw], length + 1);
     }
--- src/libraries/runtime/implementations/_start.cpp
+++ src/libraries/runtime/implementations/_start.cpp
@@ -1,16 +1,14 @@
 #include <cat/runtime>
 
 extern "C" [[gnu::used]] void _start() {
-#ifdef load_argc_argv
+#ifdef LOAD_ARGC_ARGV
     asm(R"(pop %rdi        # Load `int4 argc`.
            mov %rsp, %rsi  # Load `char* argv[]`.
-           and $-16, %rsp
-           call meow)");
-#else
+       )");
+#endif
     // The stack pointer must be aligned to prevent segfaults even in "Hello
     // World".
     cat::align_stack_pointer_32();
     meow();
-#endif
     __builtin_unreachable();  // This elides a `ret` instruction.
 }
