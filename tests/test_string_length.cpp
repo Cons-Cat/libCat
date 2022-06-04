c6d977999b244c1f6565afb291dfd5f6366e87f6
Conscat <omg-lily2000@gmail.com>
2022-06-03 23:20:21 -0400

    Improved `String::find()`.

--- tests/test_string_length.cpp
+++ tests/test_string_length.cpp
@@ -57,5 +57,10 @@ void meow() {
     ssize f = find_string.find('f').or_panic();
     Result(f == 5).or_panic();
 
+	// `z` is not inside of a 32-byte chunk.
+	    cat::String find_string_2 = "abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefz";
+		ssize z = find_string_2.find('z').or_panic();
+    Result(z == 37).or_panic();
+
     cat::exit();
 }
