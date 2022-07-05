f8df258e9940dbd82f28e97781a64cd4f59cbc0c
Conscat <omg-lily2000@gmail.com>
2022-07-05 14:46:23 -0400

    Many improvements to containers.

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,14 +1,20 @@
 #include <cat/vector>
+#include <utility>
 
 // Test that `Vector` works in a `constexpr` context.
-consteval auto const_func()->int4{
+consteval auto const_func() -> int4 {
     cat::Vector<int4> vector;
     vector.resize(8);
     vector[0] = 1;
     vector[1] = 2;
     vector[7] = 2;
     vector.push_back(10);
-	return vector[8];
+
+    // TODO: Make these work.
+    // cat::Vector vector_2 = cat::Vector<int4>::cloned(vector);
+    // cat::Vector vector_2 = cat::Vector<int4>::from(1, 2, 3);
+
+    return vector[8];
 }
 
 void meow() {
@@ -39,9 +45,13 @@ void meow() {
     Result(int_vec.size() == 0).or_panic();
     Result(int_vec.capacity() == 8).or_panic();
 
+    int_vec.resize(allocator, 4).or_panic();
+    Result(int_vec.size() == 4).or_panic();
+    Result(int_vec.capacity() == 8).or_panic();
+
     // Test reserving storage for a `Vector`.
     int_vec.reserve(allocator, 128).or_panic();
-    Result(int_vec.size() == 0).or_panic();
+    Result(int_vec.size() == 4).or_panic();
     Result(int_vec.capacity() == 128).or_panic();
 
     // Test reserve constructor.
@@ -66,6 +76,17 @@ void meow() {
     for (int4 integer : cloned_vec) {
         Result(integer == 1).or_panic();
     }
+
+    // Test from constructor.
+    cat::Vector from_vec_1 =
+        cat::Vector<int4>::from(allocator, cat::value_list<int4, 5, 10>)
+            .value();
+    Result(from_vec_1.capacity() == 10).or_panic();
+    Result(from_vec_1.size() == 10).or_panic();
+    cat::Vector from_vec_2 =
+        cat::Vector<int4>::from(allocator, 1, 2, 3, 4).value();
+    Result(from_vec_2.capacity() == 4).or_panic();
+    Result(from_vec_2.size() == 4).or_panic();
 
     // Test `Vector` in a `constexpr` context.
     static_assert(const_func() == 10);
