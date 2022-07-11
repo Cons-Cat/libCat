a1ae9b2019767f131354c379d8cda1754fd15887
Conscat <wgooch2000@gmail.com>
2022-07-11 17:15:45 -0400

    Re-enabled `[[deprecated]]` attribute on `memcpy()`.

--- src/libraries/string/implementations/memcpy.cpp
+++ src/libraries/string/implementations/memcpy.cpp
@@ -1,7 +1,6 @@
 #include <cat/string>
 
 // `__SIZE_TYPE__` is a GCC macro.
-// [[deprecated("memcpy() is deprecated! Use cat::copy_memory() instead!")]]
 extern "C" auto std::memcpy(void* p_destination, void const* p_source,
                             __SIZE_TYPE__ bytes) -> void* {
     cat::copy_memory(p_source, p_destination, static_cast<ssize>(bytes));
