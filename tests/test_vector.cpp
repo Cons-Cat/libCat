ee73d0304072c24dd20aaab99d4c422520e1a036
Conscat <omg-lily2000@gmail.com>
2022-07-12 17:43:44 -0400

    Fix heisenbug in tests with `-O3` when sanitizers and GDB disabled.

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -20,7 +20,7 @@ consteval auto const_func() -> int4 {
 auto main() -> int {
     cat::PageAllocator page_allocator;
     cat::Byte* page = page_allocator.p_malloc(4_ki).or_exit();
-    cat::LinearAllocator allocator = {page, 4_ki};
+    cat::LinearAllocator allocator = {page, 4_ki - 32};
 
     // Test default constructing a `Vector`.
     cat::Vector<int4> int_vec;
