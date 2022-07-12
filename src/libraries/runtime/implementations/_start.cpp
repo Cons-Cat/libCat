2be77502f87c9ff23bc57af5e1cffe3ea4ca85cd
Conscat <wgooch2000@gmail.com>
2022-07-12 13:36:48 -0400

    Fixed a segfault in the arrays test.

--- src/libraries/runtime/implementations/_start.cpp
+++ src/libraries/runtime/implementations/_start.cpp
@@ -3,26 +3,34 @@
 // Attributes on this prototype would have no effect.
 auto main(...) -> int;
 
-[[noreturn]] void call_main() {
-	register int argc asm("rdi");
+[[noreturn,
+// `INLINE_MAIN` can be defined in the build system to allow inlining `main()`
+// in `_start()`. This can reduce binary size in some cases, such as Hello
+// World, but is likely to cause various problems in non-trivial applications.
+#ifndef INLINE_MAIN
+  gnu::noinline
+#endif
+]] void
+call_main() {
+    register int argc asm("rdi");
     register char** p_argv asm("rsi");
     main(argc, p_argv);
     __builtin_unreachable();
 }
 
 void _start() {
-     // If `NO_ARGC_ARGV` is defined, argument loading can be skipped.
+    // `NO_ARGC_ARGV` can be defined in a build system to skip argument loading.
 #ifndef NO_ARGC_ARGV
     asm(R"(pop %rdi        # Load `int4 argc`.
            mov %rsp, %rsi  # Load `char* argv[]`.
        )");
 #endif
 
-	// The stack pointer must be aligned to prevent SIMD segfaults even in
-	// surprisingly simple programs with GCC 12.
+    // The stack pointer must be aligned to prevent SIMD segfaults even in
+    // surprisingly simple programs with GCC 12.
     cat::align_stack_pointer_32();
 
     // `main()` must be wrapped by a function to conditionally prevent inlining.
-    call_main();	
-	__builtin_unreachable();
+    call_main();
+    __builtin_unreachable();
 }
