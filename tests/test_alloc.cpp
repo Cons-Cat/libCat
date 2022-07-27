44bf98ed0c10b88de5b1951123dd28a4c113ec93
Conscat <omg-lily2000@gmail.com>
2022-07-27 15:26:21 -0400

    Tested and fixed `nalloc` function family.

--- tests/test_alloc.cpp
+++ tests/test_alloc.cpp
@@ -3,7 +3,7 @@
 #include <cat/page_allocator>
 
 struct HugeObject {
-    [[maybe_unused]] uint1 storage[256];
+    [[maybe_unused]] uint1 storage[cat::inline_buffer_size.raw + 1];
 };
 
 int4 global = 0;
@@ -16,7 +16,7 @@ struct NonTrivial {
 };
 
 struct NonTrivialHugeObject {
-    [[maybe_unused]] uint1 storage[256];
+    [[maybe_unused]] uint1 storage[cat::inline_buffer_size.raw];
     NonTrivialHugeObject() {
         ++global;
     }
@@ -275,17 +275,168 @@ auto main() -> int {
 
     // Always reset the allocator so that there are no alignment requirements
     // interfering with `nalloc()` tests. Specific allocator tests such as
-    // `test_linear_allocator.cpp` check that in greater detail.
+    // `test_linear_allocator.cpp` check that in greater cat::detail.
 
     // Test `nalloc()`.
     allocator.reset();
     ssize nalloc = allocator.nalloc<int4>().value();
-    Result(nalloc == ssizeof<int4>()).value();
+    Result(nalloc == ssizeof<int4>()).or_exit();
 
     // Test `xnalloc()`.
     allocator.reset();
     ssize xnalloc = allocator.xnalloc<int4>();
-    Result(xnalloc == ssizeof<int4>()).value();
+    Result(xnalloc == ssizeof<int4>()).or_exit();
+
+    // Test `nalloc_multi()`.
+    allocator.reset();
+    ssize nalloc_multi = allocator.nalloc_multi<int4>(5).value();
+    Result(nalloc_multi == (ssizeof<int4>() * 5)).or_exit();
+
+    // Test `xnalloc_multi()`.
+    allocator.reset();
+    ssize xnalloc_multi = allocator.xnalloc_multi<int4>(5);
+    Result(xnalloc_multi == (ssizeof<int4>() * 5)).or_exit();
+
+    // Test `align_nalloc()`.
+    allocator.reset();
+    ssize align_nalloc = allocator.align_nalloc<int4>(4u).value();
+    Result(align_nalloc == ssizeof<int4>()).or_exit();
+
+    // Test `align_xnalloc()`.
+    allocator.reset();
+    ssize align_xnalloc = allocator.align_xnalloc<int4>(4u);
+    Result(align_xnalloc == ssizeof<int4>()).or_exit();
+
+    // Test `align_nalloc_multi()`.
+    allocator.reset();
+    ssize align_nalloc_multi =
+        allocator.align_nalloc_multi<int4>(4u, 5).value();
+    Result(align_nalloc_multi == (ssizeof<int4>() * 5)).or_exit();
+
+    // Test `align_xnalloc_multi()`.
+    allocator.reset();
+    ssize align_xnalloc_multi = allocator.align_xnalloc_multi<int4>(4u, 5);
+    Result(align_xnalloc_multi == (ssizeof<int4>() * 5)).or_exit();
+
+    // Test `unalign_nalloc()`.
+    allocator.reset();
+    ssize unalign_nalloc = allocator.unalign_nalloc<int4>().value();
+    Result(unalign_nalloc == ssizeof<int4>()).or_exit();
+
+    // Test `unalign_xnalloc()`.
+    allocator.reset();
+    ssize unalign_xnalloc = allocator.unalign_xnalloc<int4>();
+    Result(unalign_xnalloc == ssizeof<int4>()).or_exit();
+
+    // Test `unalign_nalloc_multi()`.
+    allocator.reset();
+    ssize unalign_nalloc_multi =
+        allocator.unalign_nalloc_multi<int4>(5).value();
+    Result(unalign_nalloc_multi == (ssizeof<int4>() * 5)).or_exit();
+
+    // Test `unalign_xnalloc_multi()`.
+    allocator.reset();
+    ssize unalign_xnalloc_multi = allocator.unalign_xnalloc_multi<int4>(5);
+    Result(unalign_xnalloc_multi == (ssizeof<int4>() * 5)).or_exit();
+
+    // Test `inline_nalloc()`.
+    allocator.reset();
+    ssize inline_nalloc = allocator.inline_nalloc<int4>().value();
+    Result(inline_nalloc == cat::inline_buffer_size).or_exit();
+    ssize inline_nalloc_big = allocator.inline_nalloc<HugeObject>().value();
+    Result(inline_nalloc_big == 257).or_exit();
+
+    // Test `inline_xnalloc()`.
+    allocator.reset();
+    ssize inline_xnalloc = allocator.inline_xnalloc<int4>();
+    Result(inline_xnalloc == cat::inline_buffer_size).or_exit();
+    ssize inline_xnalloc_big = allocator.inline_xnalloc<HugeObject>();
+    Result(inline_xnalloc_big == 257).or_exit();
+
+    // Test `inline_nalloc_multi()`.
+    allocator.reset();
+    ssize inline_nalloc_multi = allocator.inline_nalloc_multi<int4>(5).value();
+    Result(inline_nalloc_multi == cat::inline_buffer_size).or_exit();
+    ssize inline_nalloc_multi_big =
+        allocator.inline_nalloc_multi<HugeObject>(2).value();
+    Result(inline_nalloc_multi_big == (257 * 2)).or_exit();
+
+    // Test `inline_xnalloc_multi()`.
+    allocator.reset();
+    ssize inline_xnalloc_multi = allocator.inline_xnalloc_multi<int4>(5);
+    Result(inline_xnalloc_multi == cat::inline_buffer_size).or_exit();
+    ssize inline_xnalloc_multi_big =
+        allocator.inline_xnalloc_multi<HugeObject>(2);
+    Result(inline_xnalloc_multi_big == (257 * 2)).or_exit();
+
+    // Test `inline_align_nalloc()`.
+    allocator.reset();
+    ssize inline_align_nalloc = allocator.inline_align_nalloc<int4>(4u).value();
+    Result(inline_align_nalloc == cat::inline_buffer_size).or_exit();
+    ssize inline_align_nalloc_big =
+        allocator.inline_align_nalloc<HugeObject>(1u).value();
+    Result(inline_align_nalloc_big == 257).or_exit();
+
+    // Test `inline_align_xnalloc()`.
+    allocator.reset();
+    ssize inline_align_xnalloc = allocator.inline_align_xnalloc<int4>(4u);
+    Result(inline_align_xnalloc == cat::inline_buffer_size).or_exit();
+    ssize inline_align_xnalloc_big =
+        allocator.inline_align_xnalloc<HugeObject>(1u);
+    Result(inline_align_xnalloc_big == 257).or_exit();
+
+    // Test `inline_unalign_nalloc()`.
+    allocator.reset();
+    ssize inline_unalign_nalloc =
+        allocator.inline_unalign_nalloc<int4>().value();
+    Result(inline_unalign_nalloc == cat::inline_buffer_size).or_exit();
+    ssize inline_unalign_nalloc_big =
+        allocator.inline_unalign_nalloc<HugeObject>().value();
+    Result(inline_unalign_nalloc_big == 257).or_exit();
+
+    // Test `inline_unalign_xnalloc()`.
+    allocator.reset();
+    ssize inline_unalign_xnalloc = allocator.inline_unalign_xnalloc<int4>();
+    Result(inline_unalign_xnalloc == cat::inline_buffer_size).or_exit();
+    ssize inline_unalign_xnalloc_big =
+        allocator.inline_unalign_xnalloc<HugeObject>();
+    Result(inline_unalign_xnalloc_big == 257).or_exit();
+
+    // Test `inline_align_nalloc_multi()`.
+    allocator.reset();
+    ssize inline_align_nalloc_multi =
+        allocator.inline_align_nalloc_multi<int4>(4u, 5).value();
+    Result(inline_align_nalloc_multi == cat::inline_buffer_size).or_exit();
+    ssize inline_align_nalloc_multi_big =
+        allocator.inline_align_nalloc_multi<HugeObject>(1u, 2).value();
+    Result(inline_align_nalloc_multi_big == (257 * 2)).or_exit();
+
+    // Test `inline_align_xnalloc_multi()`.
+    allocator.reset();
+    ssize inline_align_xnalloc_multi =
+        allocator.inline_align_xnalloc_multi<int4>(4u, 5);
+    Result(inline_align_xnalloc_multi == cat::inline_buffer_size).or_exit();
+    ssize inline_align_xnalloc_multi_big =
+        allocator.inline_align_xnalloc_multi<HugeObject>(1u, 2);
+    Result(inline_align_xnalloc_multi_big == (257 * 2)).or_exit();
+
+    // Test `inline_unalign_nalloc_multi()`.
+    allocator.reset();
+    ssize inline_unalign_nalloc_multi =
+        allocator.inline_unalign_nalloc_multi<int4>(5).value();
+    Result(inline_unalign_nalloc_multi == cat::inline_buffer_size).or_exit();
+    ssize inline_unalign_nalloc_multi_big =
+        allocator.inline_unalign_nalloc_multi<HugeObject>(2).value();
+    Result(inline_unalign_nalloc_multi_big == (257 * 2)).or_exit();
+
+    // Test `inline_unalign_xnalloc_multi()`.
+    allocator.reset();
+    ssize inline_unalign_xnalloc_multi =
+        allocator.inline_unalign_xnalloc_multi<int4>(5);
+    Result(inline_unalign_xnalloc_multi == cat::inline_buffer_size).or_exit();
+    ssize inline_unalign_xnalloc_multi_big =
+        allocator.inline_unalign_xnalloc_multi<HugeObject>(2);
+    Result(inline_unalign_xnalloc_multi_big == (257 * 2)).or_exit();
 
     cat::exit();
 };
