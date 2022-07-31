ddc6a21d59f79b2626521a6ae4a61885049f5c1c
Conscat <wgooch2000@gmail.com>
2022-07-30 21:52:58 -0400

    Began refactoring `<numerals>`.

---
+++ tests/test_numerals.cpp
@@ -0,0 +1,13 @@
+auto main() -> int {
+    // Test `int4` constructors and assignment.
+    int4 test_int4_1 = 1;
+    int4 test_int4_2;
+    test_int4_2 = 1;
+    int4 int4_add = 1 + test_int4_1;
+    int4_add = 1_i4 + test_int4_1;
+
+    // Test `intpr` constructors and assignment.
+    intptr<void> intptr_1 = nullptr;
+    intptr<void> intptr_2 = nullptr;
+    intptr_1 = intptr_1 + intptr_2;
+};
