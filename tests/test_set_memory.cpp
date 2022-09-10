3c848e671378e2378da32c64f4d7cf72f6a533d1
Conscat <omg-lily2000@gmail.com>
2022-09-10 16:04:16 -0400

    Made the `cat::set_memory()` family support larger values than bytes.

--- tests/test_set_memory.cpp
+++ tests/test_set_memory.cpp
@@ -6,6 +6,8 @@ auto main() -> int {
     cat::PageAllocator allocator;
     uint1* p_page = allocator.p_alloc_multi<uint1>(4_ki).or_exit();
 
+    // TODO: Make a `cat::compare_memory()` or something for this.
+
     cat::set_memory(p_page, 1_u1, 4_ki);
     Result(p_page[1000] == 1_u1).or_exit();
 
@@ -21,4 +23,19 @@ auto main() -> int {
     cat::zero_memory(p_page, 4_ki);
     Result(p_page[0] == 0_u1).or_exit();
     Result(p_page[(4_ki).raw - 1] == 0_u1).or_exit();
+
+    // Test setting values larger than 1 byte.
+    cat::set_memory(p_page, 1_i2, 2_ki);
+    Result(static_cast<int2*>(static_cast<void*>(p_page))[10] == 1_i2)
+        .or_exit();
+    // The next byte after this should be 0.
+    Result(static_cast<int1*>(static_cast<void*>(p_page))[21] == 0).or_exit();
+
+    cat::set_memory(p_page, 1_i4, 1_ki);
+    Result(static_cast<int4*>(static_cast<void*>(p_page))[10] == 1_i4)
+        .or_exit();
+
+    cat::set_memory(p_page, 1_i8, 0.5_ki);
+    Result(static_cast<int4*>(static_cast<void*>(p_page))[10] == 1_i4)
+        .or_exit();
 };
