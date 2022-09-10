5ae908b921034fa4a9b0e8a3df0e4dfe2a8e9369
Conscat <omg-lily2000@gmail.com>
2022-09-10 16:56:08 -0400

    Added a comment to `block_all_signals()`.

--- src/libraries/linux/implementations/block_all_signals.cpp
+++ src/libraries/linux/implementations/block_all_signals.cpp
@@ -2,6 +2,9 @@
 
 auto nix::block_all_signals() -> SignalsMaskSet {
     SignalsMaskSet current_mask;
+    // Most Linux runtimes use an `app_mask` here which excludes signals 32-34,
+    // which are used for something by pthreads. Because libCat doesn't use
+    // pthreads, it simply blocks all signals here, for now.
     sys_rt_sigprocmask(SignalAction::block, &all_signals_mask, &current_mask);
     return current_mask;
 }
