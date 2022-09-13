33df8545c7c7956684a044d09c02b9cf0a551a52
Conscat <omg-lily2000@gmail.com>
2022-09-13 16:02:58 -0400

    Fixed bloated hello world again.

--- src/libraries/string/implementations/print.cpp
+++ src/libraries/string/implementations/print.cpp
@@ -4,6 +4,5 @@
 auto cat::print(String const string) -> ssize {
     // There is no reasonable way for a `write` syscall for `nix::stdout` to
     // fail, except by running out of buffer space.
-    ssize const output_length = nix::sys_write(nix::stdout, string).value();
-    return output_length;
+    return nix::sys_write(nix::stdout, string).value();
 }
