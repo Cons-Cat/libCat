bf4035c580e92be2daa7e8c4712720cc0d4cac07
Conscat <omg-lily2000@gmail.com>
2022-09-27 20:36:42 -0400

    Fixed inconsistent test count between Debug and Release unit tests.

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -3,11 +3,11 @@
 #include <cat/format>
 #include <cat/page_allocator>
 
-inline cat::JmpBuffer test_jump_buffer;
+constinit cat::JmpBuffer* p_test_jump_buffer;
 
 void test_fail(cat::SourceLocation const& source_location) {
     cat::detail::print_assert_location(source_location);
-    cat::longjmp(test_jump_buffer, 1);
+    cat::longjmp(*p_test_jump_buffer, 1);
 }
 
 using Constructor = void (*)();
@@ -17,6 +17,8 @@ extern Constructor __init_array_end;    // NOLINT
 }
 
 auto main() -> int {
+    cat::JmpBuffer jump_buffer;
+    p_test_jump_buffer = &jump_buffer;
     int8 tests_passed = 0;
     int8 tests_failed = 0;
 
@@ -24,12 +26,17 @@ auto main() -> int {
     // `[[gnu::constructor]]`. The `TEST` macro declares these functions.
     Constructor* p_func = &__init_array_start;
 
+    int4 temp_tests = total_tests;
+
     // Run all unit tests:
     for (; p_func < &__init_array_end; ++p_func) {
-        if (cat::setjmp(test_jump_buffer) == 0) {
+        if (cat::setjmp(jump_buffer) == 0) {
             (*p_func)();
             // If no assert failed, run next test:
-            ++tests_passed;
+            if (temp_tests != total_tests) {
+                ++tests_passed;
+                temp_tests = total_tests;
+            }
             continue;
         }
 
@@ -43,6 +50,6 @@ auto main() -> int {
                                "{} tests passed.\n{} tests failed.\n",
                                // Subtract one, because there is one constructor
                                // call that isn't a unit test.
-                               tests_passed - 1, tests_failed)
+                               tests_passed, tests_failed)
                        .value());
 }
--- tests/unit_tests.hpp
+++ tests/unit_tests.hpp
@@ -3,6 +3,8 @@
 #include <cat/page_allocator>
 #include <cat/vector>
 
+constinit inline int4 total_tests = 0;
+
 void test_fail(cat::SourceLocation const& source_location);
 
 // This macro declares a unit test named `test_name`, which is executed
@@ -11,6 +13,7 @@ void test_fail(cat::SourceLocation const& source_location);
     void test_name();                                              \
     [[gnu::constructor]] void register_##test_name() {             \
         using cat::StaticString;                                   \
+        ++total_tests;                                             \
         StaticString string =                                      \
             "Running test: " + StaticString{#test_name} + "...\n"; \
         _ = cat::print(string);                                    \
