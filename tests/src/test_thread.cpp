bd6505ade3af51df1e689b893e57170d8cae30b8
Conscat <omg-lily2000@gmail.com>
2022-11-15 21:26:23 -0500

    Fixed `cat::Thread` test.

--- tests/src/test_thread.cpp
+++ tests/src/test_thread.cpp
@@ -7,25 +7,23 @@
 
 #include "../unit_tests.hpp"
 
+// TODO: Test this without I/O.
+
 void function(void*) {
     for (int4 i = 0; i < 15; ++i) {
-        _ = cat::println("Moo?");
+        // _ = cat::println("Moo?");
     }
     cat::exit();
 }
 
-// ASan causes this program to hang with a mysterious call to
-// `AsanOnDeadlySignal()`.
-[[gnu::no_sanitize_address]] TEST(test_thread) {
+TEST(test_thread) {
     cat::Thread thread;
     cat::PageAllocator allocator;
     thread.create(allocator, 2_ki, function, nullptr)
         .or_exit("Failed to make thread!");
     for (int4 i = 0; i < 10; ++i) {
-        _ = cat::println("Boo!");
+        // _ = cat::println("Boo!");
     }
     thread.join().or_exit("Failed to join thread!");
-    _ = cat::println("Finished!");
-    // TODO: ASan prints `AddressSanitizer:DEADLYSIGNAL` without this call:
-    cat::exit();
+    // _ = cat::println("Finished!");
 }
