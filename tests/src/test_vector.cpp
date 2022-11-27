204c92803598c344a96807798f63a6155da77d9b
Conscat <omg-lily2000@gmail.com>
2022-11-26 23:19:56 -0500

    Optimize `cat::Vector` with allocators' size feedback.

--- tests/src/test_vector.cpp
+++ tests/src/test_vector.cpp
@@ -20,7 +20,8 @@ TEST(test_vector) {
     // Initialize an allocator.
     cat::PageAllocator paging_allocator;
     paging_allocator.reset();
-    auto page = paging_allocator.opq_alloc_multi<cat::Byte>(4_ki - 32).or_exit();
+    auto page =
+        paging_allocator.opq_alloc_multi<cat::Byte>(4_ki - 32).or_exit();
     DEFER(paging_allocator.free(page);)
     auto allocator =
         cat::LinearAllocator::backed_handle(paging_allocator, page);
@@ -28,45 +29,45 @@ TEST(test_vector) {
     // Test default constructing a `Vector`.
     cat::Vector<int4> int_vec;
     cat::verify(int_vec.size() == 0);
-    cat::verify(int_vec.capacity() == 0);
+    cat::verify(int_vec.capacity() >= 0);
 
     // Test pushing back to a `Vector`.
     int_vec.push_back(allocator, 1).or_exit();
     int_vec.push_back(allocator, 2).or_exit();
     int_vec.push_back(allocator, 3).or_exit();
     cat::verify(int_vec.size() == 3);
-    cat::verify(int_vec.capacity() == 4);
+    cat::verify(int_vec.capacity() >= 4);
 
     int_vec.push_back(allocator, 6).or_exit();
     int_vec.push_back(allocator, 12).or_exit();
     int_vec.push_back(allocator, 24).or_exit();
     cat::verify(int_vec.size() == 6);
-    cat::verify(int_vec.capacity() == 8);
+    cat::verify(int_vec.capacity() >= 8);
 
     // Test resizing a `Vector`.
     int_vec.resize(allocator, 0).or_exit();
     cat::verify(int_vec.size() == 0);
-    cat::verify(int_vec.capacity() == 8);
+    cat::verify(int_vec.capacity() >= 8);
 
     int_vec.resize(allocator, 4).or_exit();
     cat::verify(int_vec.size() == 4);
-    cat::verify(int_vec.capacity() == 8);
+    cat::verify(int_vec.capacity() >= 8);
 
     // Test reserving storage for a `Vector`.
     int_vec.reserve(allocator, 128).or_exit();
     cat::verify(int_vec.size() == 4);
-    cat::verify(int_vec.capacity() == 128);
+    cat::verify(int_vec.capacity() >= 128);
 
     // Test reserve constructor.
     cat::Vector reserved_vec =
         cat::Vector<int4>::reserved(allocator, 6).or_exit();
-    cat::verify(reserved_vec.capacity() == 6);
+    cat::verify(reserved_vec.capacity() >= 6);
 
     // Test filled constructor.
     cat::Vector filled_vec =
         cat::Vector<int4>::filled(allocator, 8, 1).or_exit();
     cat::verify(filled_vec.size() == 8);
-    cat::verify(filled_vec.capacity() == 8);
+    cat::verify(filled_vec.capacity() >= 8);
     for (int4 integer : filled_vec) {
         cat::verify(integer == 1);
     }
@@ -74,7 +75,7 @@ TEST(test_vector) {
     // Test cloned constructor.
     cat::Vector cloned_vec = filled_vec.clone(allocator).or_exit();
     cat::verify(cloned_vec.size() == 8);
-    cat::verify(cloned_vec.capacity() == 8);
+    cat::verify(cloned_vec.capacity() >= 8);
     for (int4 integer : cloned_vec) {
         cat::verify(integer == 1);
     }
@@ -85,16 +86,22 @@ TEST(test_vector) {
     // Test getters.
     cat::Vector<int> default_vector;
     cat::verify(default_vector.is_empty());
-    cat::verify(default_vector.is_full());
 
     _ = default_vector.reserve(allocator, 2);
     cat::verify(default_vector.is_empty());
-    cat::verify(!default_vector.is_full());
 
     _ = default_vector.push_back(allocator, 0);
     _ = default_vector.push_back(allocator, 0);
     cat::verify(!default_vector.is_empty());
+
+    // Resize the vector to be larger, then check it's full.
+    _ = default_vector.resize(allocator, default_vector.capacity() + 1)
+            .verify();
     cat::verify(default_vector.is_full());
+
+    // Resize the vector to be smaller, then check it's not full.
+    _ = default_vector.resize(allocator, 2).verify();
+    cat::verify(!default_vector.is_full());
 
     // TODO: Test insert iterators.
 
