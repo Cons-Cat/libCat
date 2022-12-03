901996e8d3e7c965f990d4fa5f5b55b712076973
Conscat <omg-lily2000@gmail.com>
2022-12-03 13:14:02 -0500

    Implemented allocation and free for `cat::PoolAllocator`.

--- tests/src/test_pool_allocator.cpp
+++ tests/src/test_pool_allocator.cpp
@@ -10,5 +10,36 @@ TEST(test_pool_allocator) {
     cat::is_stable_allocator auto allocator =
         cat::PoolAllocator<8>::backed(paging_allocator, 128).value();
 
-    int4* p_int = allocator.alloc<int4>().value();
+    // Make an allocation.
+    int4* p_int1 = allocator.alloc<int4>(10).value();
+    cat::verify(*p_int1 == 10);
+
+    // Make another allocation.
+    int4* p_int2 = allocator.alloc<int4>(20).value();
+    cat::verify(*p_int2 == 20);
+
+    // Test allocation limit is correct.
+    for (int4 i = 0; i < (128 / 8) - 2; ++i) {
+        _ = allocator.alloc<int4>().verify();
+    }
+    cat::Maybe failed_allocation = allocator.alloc<int4>();
+    cat::verify(!failed_allocation.has_value());
+
+    // Make an allocation after resetting.
+    allocator.reset();
+    int4* p_int3 = allocator.alloc<int4>(30).verify();
+    cat::verify(*p_int3 == 30);
+
+    // Test freeing memory.
+    for (int4 i = 0; i < (128 / 8) - 1; ++i) {
+        _ = allocator.alloc<int4>().verify();
+    }
+    failed_allocation = allocator.alloc<int4>();
+    cat::verify(!failed_allocation.has_value());
+
+    allocator.free(p_int3);
+
+    // Make one allocation after freeing.
+    int4* p_int4 = allocator.alloc<int4>(40).verify();
+    cat::verify(*p_int4 == 40);
 }
