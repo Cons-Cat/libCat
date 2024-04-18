f49a8a4c9f40675b8e00140967d1286e272f9d4b
Lilian Gooch <omg.lily2000@gmail.com>
2024-04-18 16:54:28 -0500

    Comment fix.

--- format.cmake
+++ format.cmake
@@ -1,11 +1,12 @@
 # This script is executed by `CMakeLists.txt` for the `cat-format` target.
 
-# `CMAKE_ARGV4` is a space-delimited list of files to format.
+# `CMAKE_ARGV4` is a space-delimited sequence of files to format.
+# Spaces are replaced by semicolons to make this a list.
 string(REPLACE " " ";" files ${CMAKE_ARGV4})
 foreach(file IN ITEMS ${files})
-    # `CMAKE_ARGV3` is a path to a `clang-format` executable, specified
-    # By the `CAT_CLANG_FORMAT_PATH` variable.
-    execute_process(
+  # `CMAKE_ARGV3` is a path to a `clang-format` executable, specified
+  # By the `CAT_CLANG_FORMAT_PATH` variable.
+  execute_process(
     COMMAND ${CMAKE_ARGV3} -i ${file}
   )
 endforeach()
