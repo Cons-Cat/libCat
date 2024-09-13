54d371bc51461eca8d9ad5c5695bb90cf9f24b38
Lilian Gooch <omg.lily2000@gmail.com>
2024-09-13 17:28:04 -0500

    More Clang build fixes.

---
+++ src/libraries/runtime/implementations/__cxa_pure_virtual.cpp
@@ -0,0 +1,8 @@
+#include <cat/runtime>
+#include <cat/string>
+
+// This function is a stub.
+extern "C" void
+cat::__cxa_pure_virtual() {
+    verify(false, "A pure virtual function was called with no implementation.");
+}
