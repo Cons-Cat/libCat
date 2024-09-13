60b9802d3ce31cf3112b0f93a5cbcd19695f6232
Lilian Gooch <omg.lily2000@gmail.com>
2024-09-13 12:40:55 -0500

    Fix LTO errors with Clang.

--- src/libraries/linux/implementations/tty_get_attributes.cpp
+++ src/libraries/linux/implementations/tty_get_attributes.cpp
@@ -4,7 +4,9 @@ auto
 nix::tty_get_attributes(file_descriptor tty)
     -> cat::scaredy<tty_io_serial, linux_error> {
     // `&configuration` is an output parameter.
+#ifndef __clang__
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
     tty_io_serial configuration;
     cat::scaredy result = sys_ioctl(tty, io_requests::tcgets, &configuration);
 
