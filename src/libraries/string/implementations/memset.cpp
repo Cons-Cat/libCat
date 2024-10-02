b7ef339f131fcf1a71ad9674bf877024d9657bc3
Lilian Gooch <omg.lily2000@gmail.com>
2024-10-02 17:14:15 -0500

    Fix `memset` being implemented incorrectly for Clang.

--- src/libraries/string/implementations/memset.cpp
+++ src/libraries/string/implementations/memset.cpp
@@ -8,8 +8,9 @@ std::memset(void* p_source, int byte_value, __SIZE_TYPE__ bytes) -> void* {
     // assignment operators. The vectorized `set_memory` causes some
     // problems with that, so this trivial loop is done insead. Find a
     // better solution.
-    for (unsigned i = 0; i < bytes; ++i) {
-        *((unsigned char*)p_source) = byte_value;
+    for (unsigned long long i = 0; i < bytes; ++i) {
+        *(static_cast<unsigned char*>(p_source) + i) =
+            static_cast<unsigned char>(byte_value);
     }
 #else
     cat::set_memory(p_source, static_cast<unsigned char>(byte_value), bytes);
