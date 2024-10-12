b2dc942c4853c35da838fd83978811a347edcb00
Lilian Gooch <omg.lily2000@gmail.com>
2024-10-12 18:01:20 -0500

    Many improvements to cpuid feature detection.

--- tests/src/test_cpuid.cpp
+++ tests/src/test_cpuid.cpp
@@ -1,6 +1,14 @@
 #include "../unit_tests.hpp"
 
 TEST(test_cpuid) {
-   bool has_avx2 = __builtin_cpu_supports("avx2");
-   cat::verify(has_avx2);
+   cat::verify(__builtin_cpu_supports("cmov"));
+   cat::verify(__builtin_cpu_supports("mmx"));
+   cat::verify(__builtin_cpu_supports("sse"));
+   cat::verify(__builtin_cpu_supports("sse2"));
+
+   cat::verify(__builtin_cpu_supports("avx2"));
+
+   cat::verify(__builtin_cpu_supports("x86-64"));
+   cat::verify(__builtin_cpu_supports("x86-64-v2"));
+   cat::verify(__builtin_cpu_supports("x86-64-v3"));
 }
