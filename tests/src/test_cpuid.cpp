4fbd5b50ada2d048c0d6d9e38b0f68032dcaa872
Lilian Gooch <omg.lily2000@gmail.com>
2024-10-13 19:51:52 -0500

    Record CPU vendor, family, type, and subtype.

--- tests/src/test_cpuid.cpp
+++ tests/src/test_cpuid.cpp
@@ -8,6 +8,8 @@ TEST(test_cpuid) {
 
    cat::verify(__builtin_cpu_supports("avx2"));
 
+   cat::verify(__builtin_cpu_is("skylake"));
+
    cat::verify(__builtin_cpu_supports("x86-64"));
    cat::verify(__builtin_cpu_supports("x86-64-v2"));
    cat::verify(__builtin_cpu_supports("x86-64-v3"));
