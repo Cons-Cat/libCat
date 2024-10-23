412254b06b49f59885a301b0b11f20b15ee36357
Lilian Gooch <omg.lily2000@gmail.com>
2024-10-23 16:14:06 -0500

    With the new `vec`, generalize static constructor function calls.

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -12,14 +12,13 @@ inline constinit cat::jmp_buffer* p_jump_buffer = nullptr;
 namespace cat {
 void
 test_fail(cat::source_location const& source_location) {
-    cat::detail::print_assert_location(source_location);
-    auto _ = cat::println();
-    ++tests_failed;
-    cat::longjmp(*p_jump_buffer, 2);
+   cat::detail::print_assert_location(source_location);
+   auto _ = cat::println();
+   ++tests_failed;
+   cat::longjmp(*p_jump_buffer, 2);
 }
 }  // namespace cat
 
-using constructor_fn = void (*const)();
 extern "C" {
 extern constructor_fn __init_array_start[];  // NOLINT
 extern constructor_fn __init_array_end[];    // NOLINT
@@ -27,35 +26,28 @@ extern constructor_fn __init_array_end[];    // NOLINT
 
 auto
 main() -> int {
-    // Change the default assert handler.
-    cat::assert_handler = &cat::test_fail;
+   // Change the default assert handler.
+   cat::assert_handler = &cat::test_fail;
 
-    // Set the jump buffer pointer before any constructors are called.
-    cat::jmp_buffer jump_buffer;
-    p_jump_buffer = &jump_buffer;
+   // Set the jump buffer pointer before any constructors are called.
+   cat::jmp_buffer jump_buffer;
+   p_jump_buffer = &jump_buffer;
 
-    // Load and call all functions with the attribute `[[gnu::constructor]]`.
-    // The `TEST` macro declares these functions.
+   // Call all constructor functions, including unit tests:
+   // for (constructor_fn const* pp_ctor_func = __init_array_start;
+   for (cat::idx i = 0; i < test_fns.size(); ++i) {
+      auto _ = ::cat::print(::cat::fmt(pager, "Running test {}", i).value());
+      if (cat::setjmp(jump_buffer)) {
+         // Jump here when a test fails, skipping the rest of a test's
+         // constructor function.
+         continue;
+      }
+      (reinterpret_cast<constructor_fn>(test_fns[i]))();
+   }
 
-    // Call all constructor functions, including unit tests:
-    for (constructor_fn const* pp_ctor_func = __init_array_start;
-         pp_ctor_func < __init_array_end; ++pp_ctor_func) {
-        last_ctor_was_test = false;
-        if (cat::setjmp(jump_buffer)) {
-            // Jump here when a test fails, skipping the rest of a test's
-            // constructor function.
-            continue;
-        }
-
-        constructor_fn p_ctor = *pp_ctor_func;
-        // If this constructor is a unit test, it sets the previous flags.
-        p_ctor();
-    }
-
-    // `tests_passed` and `tests_failed` are modified within the `TEST` macro.
-    // TODO: This will leak. An `inline_allocator` should be used.
-    auto _ =
-        cat::print(cat::fmt(pager, "\n{} tests passed.\n{} tests failed.\n",
-                            tests_passed, tests_failed)
-                       .or_exit());
+   // `tests_passed` and `tests_failed` are modified within the `TEST` macro.
+   // TODO: This will leak. An `inline_allocator` should be used.
+   auto _ = cat::print(cat::fmt(pager, "\n{} tests passed.\n{} tests failed.\n",
+                                tests_passed, tests_failed)
+                          .or_exit());
 }
