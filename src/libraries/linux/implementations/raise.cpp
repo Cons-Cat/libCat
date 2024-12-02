77498690c8c91fd7bfa68d01c235e6bb8a8f5640
Lilian Gooch <omg.lily2000@gmail.com>
2024-12-02 11:57:50 -0600

    Various fixes to enum warnings.

--- src/libraries/linux/implementations/raise.cpp
+++ src/libraries/linux/implementations/raise.cpp
@@ -1,7 +1,7 @@
 #include <cat/linux>
 
 auto
-nix::raise(Signal signal, process_id pid) -> scaredy_nix<void> {
+nix::raise(signal signal, process_id pid) -> scaredy_nix<void> {
    signals_mask_set current_mask = block_all_signals();
    cat::scaredy result = sys_tkill(pid, signal);
    sys_rt_sigprocmask(signal_action::set_mask, &current_mask, nullptr);
