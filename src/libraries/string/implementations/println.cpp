ce805831ccd09f0bb0aaa1d5f7dcb22dc3ebb9c9
Lilian Gooch <omg.lily2000@gmail.com>
2024-12-03 14:48:48 -0600

    Additional fixes and improvements to `cat::vec`.

--- src/libraries/string/implementations/println.cpp
+++ src/libraries/string/implementations/println.cpp
@@ -1,8 +1,9 @@
 #include <cat/linux>
 #include <cat/string>
 
+// TODO: Return `idx` here.
 auto
-cat::println(str_view const string) -> iword {
+cat::println(str_view string) -> iword {
    // There is no reasonable way for a `write` syscall for `nix::stdout` to
    // fail, except by running out of buffer space, which fails gracefully
    // anyways.
