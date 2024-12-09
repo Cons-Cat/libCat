4ca44fd98d4f7e219f3544a7bfac1825ce4eb709
Lilian Gooch <omg.lily2000@gmail.com>
2024-12-09 15:37:40 -0600

    Make `cat::vec` and `cat::list` RAII more consistent.

--- src/libraries/linux/implementations/sys_munmap.cpp
+++ src/libraries/linux/implementations/sys_munmap.cpp
@@ -4,5 +4,6 @@
 auto
 nix::sys_munmap(void const* p_memory, cat::uword length)
    -> nix::scaredy_nix<void> {
+   poison_memory_region(p_memory, length);
    return nix::syscall<void>(11, p_memory, length);
 }
