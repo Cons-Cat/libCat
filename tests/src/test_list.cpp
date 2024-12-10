1debc89f5a9e74bdf7cfc762c404c2c96eb26424
Lilian Gooch <omg.lily2000@gmail.com>
2024-12-09 22:33:50 -0600

    Consolidate doubly and singly linked `list`s into one template.

--- tests/src/test_list.cpp
+++ tests/src/test_list.cpp
@@ -147,46 +147,44 @@ test(list) {
    cat::verify(!null_list.emplace_back(1).has_value());
    cat::verify(!null_list.emplace_front(1).has_value());
 
-   // Test `forward_list`.
-   /*
-   allocator.reset();
-   cat::forward_list<int4> forward_list_1;
-   auto _ = forward_list_1.push_front(0).verify();
-   auto _ = forward_list_1.emplace_front(1).verify();
-   auto _ = forward_list_1.insert_after(forward_list_1.begin() + 1, 2).verify();
-   auto _ = forward_list_1.emplace_after(forward_list_1.end(), 3).verify();
+   // Test `slist`.
+   cat::slist slist_1 = cat::make_slist<int4>(allocator).verify();
+   auto _ = slist_1.push_front(0).verify();
+   auto _ = slist_1.emplace_front(1).verify();
+   // auto _ = slist_1.insert_after(slist_1.begin() + 1, 2).verify();
+   // auto _ = slist_1.emplace_after(slist_1.end(), 3).verify();
 
-   cat::verify(*forward_list_1.begin() == 1);
-   cat::verify(*(forward_list_1.begin() + 1) == 0);
-   cat::verify(*(forward_list_1.begin() + 2) == 2);
-   cat::verify(*(forward_list_1.begin() + 3) == 3);
+   cat::verify(*slist_1.begin() == 1);
+   cat::verify(*(slist_1.begin() + 1u) == 0);
+   // cat::verify(*(slist_1.begin() + 2) == 2);
+   // cat::verify(*(slist_1.begin() + 3) == 3);
 
-   // Deep copy a `forward_list`.
-   cat::forward_list<int4> forward_list_2 =
-      forward_list_1.clone(allocator).verify();
-   auto forward_it_1 = forward_list_1.begin();
-   auto forward_it_2 = forward_list_2.begin();
+   // Deep copy a `slist`.
+   cat::slist slist_2 = slist_1.clone(allocator).verify();
+   auto forward_it_1 = slist_1.begin();
+   auto forward_it_2 = slist_2.begin();
 
    // Prove all elements copied correctly.
-   while (forward_it_1 != forward_list_1.end()) {
+   cat::verify(slist_1.size() == 2);
+   cat::verify(slist_2.size() == 2);
+   for (idx i = 0; i < slist_2.size(); ++i) {
       cat::verify(*forward_it_1 == *forward_it_2);
       ++forward_it_1;
       ++forward_it_2;
    }
 
-   // Remove elements from `forward_list`.
-   forward_list_1.erase_after(forward_list_1.begin());
-   cat::verify(*(forward_list_1.begin() + 1) == 2);
+   // Remove elements from `slist`.
+   // slist_1.erase_after(slist_1.begin());
+   // cat::verify(*(slist_1.begin() + 1) == 2);
 
-   forward_list_1.pop_front(allocator);
-   cat::verify(*forward_list_1.begin() == 2);
+   slist_1.pop_front();
+   // cat::verify(*slist_1.begin() == 2);
 
    // Test that the copy was deep, after modifying the original.
-   cat::verify(*(forward_list_2.begin()) == 1);
-   cat::verify(*(forward_list_2.begin() + 1) == 0);
-   cat::verify(*(forward_list_2.begin() + 2) == 2);
-   cat::verify(*(forward_list_2.begin() + 3) == 3);
-   */
+   // cat::verify(*(slist_2.begin()) == 1);
+   // cat::verify(*(slist_2.begin() + 1) == 0);
+   // cat::verify(*(slist_2.begin() + 2) == 2);
+   // cat::verify(*(slist_2.begin() + 3) == 3);
 
    // Test `back_insert_iterator`.
    cat::list back_list = cat::make_list<int4>(allocator).verify();
