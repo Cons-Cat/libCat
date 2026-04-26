4c5c08204b87180f436f17435c11461d94695e04
Cons-Cat <omg.lily2000@gmail.com>
2026-04-25 21:19:36 -0700

    Reject narrowing `idx`/`uintptr` constant constructor calls.

--- cmake/cat_arithmetic_neg_matrix.cmake
+++ cmake/cat_arithmetic_neg_matrix.cmake
@@ -1,8 +1,8 @@
 # Exercises the full ill-formed `cat::[u]int{1,2,4,8}` cross-width and `int` to
 # `float{2,4,8}` and `iword` on `int4` grids that the unit `can_*_assign` story
 # allows as `-fsyntax-only` TUs. `index` and `arithmetic_ptr` are not a width
-# product. The named probes for those stay in `cat_arithmetic_neg_cases.cmake`
-# Rely on `_cat_neg_try_fails` in `cat_arithmetic_negative.cmake`
+# product. The named probes for those stay in `cat_arithmetic_neg_cases.cmake`.
+# Uses `_cat_neg_expect_illformed` from `cat_arithmetic_negative.cmake`.
 
 set(_m_hdr [[#include <cat/arithmetic>
 #include "arithmetic_neg_deconst.hpp"
@@ -21,7 +21,7 @@ foreach(ua IN ITEMS 1 2 4 8)
         "${_m_hdr}void t() { cat::uint@ua@ u(0u); cat::int@ib@ x(0);
   u = cat_neg_probe::deconst_number(x); }"
         _src @ONLY)
-      _cat_neg_try_fails("${_z}" "${_src}")
+      _cat_neg_expect_illformed("${_z}" "${_src}")
     endif()
   endforeach()
   foreach(ub IN ITEMS 1 2 4 8)
@@ -33,7 +33,7 @@ foreach(ua IN ITEMS 1 2 4 8)
         "${_m_hdr}void t() { cat::int@ua@ a(0); cat::uint@ub@ b(0u);
   a = cat_neg_probe::deconst_number(b); }"
         _src2 @ONLY)
-      _cat_neg_try_fails("${_z2}" "${_src2}")
+      _cat_neg_expect_illformed("${_z2}" "${_src2}")
     endif()
   endforeach()
 endforeach()
@@ -49,13 +49,13 @@ foreach(_p IN LISTS _cat_neg_narrow)
     "${_m_hdr}void t() { cat::int@l@ w(cat_neg_probe::deconst_number(0));
   cat::int@s@ a = w; }"
     _n1 @ONLY)
-  _cat_neg_try_fails("${_nmid}" "${_n1}")
+  _cat_neg_expect_illformed("${_nmid}" "${_n1}")
   string(CONFIGURE "icvt-impl-narrow-to-uint@s@-uint@l@" _nmid2 @ONLY)
   string(CONFIGURE
     "${_m_hdr}void t() { cat::uint@l@ w(cat_neg_probe::deconst_number(0u));
   cat::uint@s@ a = w; }"
     _n2 @ONLY)
-  _cat_neg_try_fails("${_nmid2}" "${_n2}")
+  _cat_neg_expect_illformed("${_nmid2}" "${_n2}")
   unset(_nmid)
   unset(_nmid2)
 endforeach()
@@ -70,7 +70,7 @@ foreach(ia IN ITEMS 1 2 4 8)
       "${_m_hdr}void t() { cat::int@ia@ i(cat_neg_probe::deconst_number(0));
   cat::uint@ub@ u = i; }"
       _mx1 @ONLY)
-    _cat_neg_try_fails("${_mxn}" "${_mx1}")
+    _cat_neg_expect_illformed("${_mxn}" "${_mx1}")
     unset(_mxn)
   endforeach()
 endforeach()
@@ -89,7 +89,7 @@ foreach(ib IN ITEMS 1 2 4 8)
         "${_m_hdr}void t() { cat::uint@ua@ w(cat_neg_probe::deconst_number(0u));
   cat::int@ib@ a = w; }"
         _mx2 @ONLY)
-      _cat_neg_try_fails("${_mxn2}" "${_mx2}")
+      _cat_neg_expect_illformed("${_mxn2}" "${_mx2}")
       unset(_mxn2)
     endif()
   endforeach()
@@ -100,17 +100,17 @@ string(CONFIGURE
   "${_m_hdr}void t() { int v = cat_neg_probe::deconst_number(1);
   cat::float2 f = v; }"
   _f2 @ONLY)
-_cat_neg_try_fails("icvt-impl-int-to-float2" "${_f2}")
+_cat_neg_expect_illformed("icvt-impl-int-to-float2" "${_f2}")
 string(CONFIGURE
   "${_m_hdr}void t() { int v = cat_neg_probe::deconst_number(1);
   cat::float4 f = v; }"
   _f4 @ONLY)
-_cat_neg_try_fails("icvt-impl-int-to-float4" "${_f4}")
+_cat_neg_expect_illformed("icvt-impl-int-to-float4" "${_f4}")
 string(CONFIGURE
   "${_m_hdr}void t() { int v = cat_neg_probe::deconst_number(1);
   cat::float8 f = v; }"
   _f8 @ONLY)
-_cat_neg_try_fails("icvt-impl-int-to-float8" "${_f8}")
+_cat_neg_expect_illformed("icvt-impl-int-to-float8" "${_f8}")
 unset(ua)
 unset(ib)
 
@@ -128,31 +128,31 @@ foreach(_p IN LISTS _cat_neg_narrow)
     "${_m_hdr2}void t() { cat::int@s2@ a{}; a += cat_neg_probe::deconst_number(
     cat::int@l2@{0}); }"
     _c1 @ONLY)
-  _cat_neg_try_fails("${_cid1}" "${_c1}")
+  _cat_neg_expect_illformed("${_cid1}" "${_c1}")
   string(CONFIGURE "muleq-int@s2@-int@l2@" _cid2 @ONLY)
   string(CONFIGURE
     "${_m_hdr2}void t() { cat::int@s2@ a{}; a *= cat_neg_probe::deconst_number(
     cat::int@l2@{0}); }"
     _c2 @ONLY)
-  _cat_neg_try_fails("${_cid2}" "${_c2}")
+  _cat_neg_expect_illformed("${_cid2}" "${_c2}")
   string(CONFIGURE "minuseq-int@s2@-int@l2@" _cid3 @ONLY)
   string(CONFIGURE
     "${_m_hdr2}void t() { cat::int@s2@ a{}; a -= cat_neg_probe::deconst_number(
     cat::int@l2@{0}); }"
     _c3 @ONLY)
-  _cat_neg_try_fails("${_cid3}" "${_c3}")
+  _cat_neg_expect_illformed("${_cid3}" "${_c3}")
   string(CONFIGURE "pluseq-uint@s2@-uint@l2@" _cid4 @ONLY)
   string(CONFIGURE
     "${_m_hdr2}void t() { cat::uint@s2@ a{}; a += cat_neg_probe::deconst_number(
     cat::uint@l2@{0u}); }"
     _c4 @ONLY)
-  _cat_neg_try_fails("${_cid4}" "${_c4}")
+  _cat_neg_expect_illformed("${_cid4}" "${_c4}")
   string(CONFIGURE "undef-minuseq-int@s2@-int@l2@" _cid5 @ONLY)
   string(CONFIGURE
     "${_m_hdr2}void t() { cat::int@s2@ a{}; a.undef() -=
     cat_neg_probe::deconst_number(cat::int@l2@{0}); }"
     _c5 @ONLY)
-  _cat_neg_try_fails("${_cid5}" "${_c5}")
+  _cat_neg_expect_illformed("${_cid5}" "${_c5}")
   unset(_cid1)
   unset(_cid2)
   unset(_cid3)
@@ -167,7 +167,7 @@ string(CONFIGURE
   "${_m_hdr2}void t() { cat::int4 a{}; cat::iword w(0);
   a -= cat_neg_probe::deconst_number(w); }"
   _iwe @ONLY)
-_cat_neg_try_fails("minuseq-int4-iword" "${_iwe}")
+_cat_neg_expect_illformed("minuseq-int4-iword" "${_iwe}")
 unset(_m_hdr)
 unset(_m_hdr2)
 unset(_c1)
