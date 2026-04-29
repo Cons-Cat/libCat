67bdcc5359bb55191e5c54bfb967d71db2b61fc1
Cons-Cat <omg.lily2000@gmail.com>
2026-04-29 16:14:29 -0700

    Detect missing source files in CI.

--- src/libraries/linux/implementations/sys_creat.cpp
+++ src/libraries/linux/implementations/sys_creat.cpp
@@ -2,9 +2,12 @@
 // vim: set ft=cpp:
 #include <cat/linux>
 
-auto nix::sys_creat(char const* p_file_path, nix::open_mode file_mode)
-    -> nix::scaredy_nix<nix::file_descriptor> {
-    return nix::sys_open(p_file_path, file_mode,
-                         nix::open_flags::create | nix::open_flags::truncate |
-                             nix::open_mode::read_write);
+auto
+nix::sys_creat(char const* p_file_path, nix::open_mode file_mode)
+   -> nix::scaredy_nix<file_descriptor> {
+   return nix::sys_open(p_file_path, file_mode,
+                        open_flags::create
+                           | nix::open_flags::truncate
+                           | static_cast<nix::open_flags>(
+                              cat::to_underlying(nix::open_mode::read_write)));
 }
--- src/libraries/runtime/implementations/__dso_handle.cpp
+++
@@ -1,7 +0,0 @@
-#include <cat/runtime>
-
-// This function is a stub.
-
-extern "C" auto cat::__dso_handle(void (*)(void*), void*, void*) -> int {
-    return 0;
-}
