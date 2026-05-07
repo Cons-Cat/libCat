06238b743580d5de830d3afa5daefbe3bc96596b
Cons-Cat <omg.lily2000@gmail.com>
2026-05-07 01:01:27 -0700

    Optimize `echo`.

--- examples/echo.cpp
+++ examples/echo.cpp
@@ -1,21 +1,33 @@
 #include <cat/linux>
 
+// This is a minimal echo of equal size to 64-bit:
+// https://blog.mandejan.nl/posts/smallest-echo.html
+//
+// With linker optimizations, we actually get 355 bytes.
+
 auto
 main(int argc, char* p_argv[]) -> int {
-   // Skip the 0th argument, the program name.
-   for (cat::idx i = 1; i < argc; ++i) {
-      cat::idx length = 0;
-      // TODO: Use a `string_length_scalar()`.
-      while (p_argv[i][length] != 0) {
-         ++length;
-      }
-
-      // Replace the null terminator with a space.
-      p_argv[i][length] = ' ';
-      ++length; // Include that space in print output.
-      cat::str_view string = {p_argv[i], length };
-      auto _ = cat::print(string);
+   if (argc <= 1) {
+      auto _ = cat::println();
+      return 0;
    }
 
-   auto _ = cat::println();
+   // Linux lays out argv strings contiguously so we replace null terminators
+   // with spaces and the final null with a newline, then print the whole
+   // string in a single string.
+   // TODO: Replace this with a substring replace function.
+   char* p_end = p_argv[1];
+   for (cat::idx i = 1; i < argc; ++i) {
+      while (*p_end != 0) {
+         ++p_end;
+         // Prevent a large `vpcmpistri` sequence that bloats `.text` by ~200 bytes.
+         // Currently libCat struggles with the attributes on `main()` to fix this.
+         asm volatile("" : "+r"(p_end));
+      }
+      *p_end++ = (i == argc - 1) ? '\n' : ' ';
+   }
+
+   cat::idx length{p_end - p_argv[1]};
+   cat::str_view line = {p_argv[1], length};
+   auto _ = cat::print(line);
 }
