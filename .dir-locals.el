0c86f9b82f791e597dc5136b3f25581d837f7c71
Lilian Gooch <omg.lily2000@gmail.com>
2026-09-23 20:56:04 -0700

    Improve `.dir-locals.el`.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,10 @@
          (tab-width . 3)
          (c-basic-offset . 3)
          (c-ts-mode-indent-offset . 3)
-         (fill-column . 80))))
+         (fill-column . 80)
+         (compile-command . "just build")
+         (eval . (setq-local lsp-clients-clangd-executable
+                             (expand-file-name
+                              ".cache/cat-llvm/bin/clangd"
+                              (locate-dominating-file default-directory
+                                                      ".dir-locals.el")))))))
